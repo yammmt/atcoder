e885b17ec115feea8cc703e79f777c94360468bf
yammmt <cu.cs.y20@gmail.com>
2020-07-06 21:33:00 +0900

    :feet:

--- abc173/abc173b/src/main.rs
+++ abc173/abc173b/src/main.rs
@@ -14,11 +14,11 @@ fn main() {
     let mut re = 0;
     for i in s {
         match i.as_ref() {
-            ("AC") => ac += 1,
-            ("WA") => wa += 1,
-            ("TLE") => tle += 1,
-            ("RE") => re += 1,
-            (_) => {},
+            "AC" => ac += 1,
+            "WA" => wa += 1,
+            "TLE" => tle += 1,
+            "RE" => re += 1,
+            _ => {},
         }
     }
     println!("AC x {}\nWA x {}\nTLE x {}\nRE x {}", ac, wa, tle, re);
--- abc173/abc173c/src/main.rs
+++ abc173/abc173c/src/main.rs
@@ -1,87 +1,59 @@
 // -*- coding:utf-8-unix -*-
 
-// NOT WORK
-
-use itertools::Itertools;
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
     input! {
-        h: usize,
-        w: usize,
-        k: usize,
-        c: [String; h],
+        h: u32,
+        w: u32,
+        k: u32,
+        c: [Chars; h],
     }
 
-    let mut total_black = 0;
+    let mut total_black: u32 = 0;
     for i in &c {
-        total_black += i.chars().filter(|&a| a == '#').count();
+        total_black += i.into_iter().filter(|&a| a == &'#').count() as u32;
     }
+    // println!("{}", total_black);
 
     let mut ans = 0;
-    for row_num in 0..h + 1 {
-        // println!("h: {}", h);
-        let mut current_black = total_black;
-        let mut current_table = c.clone();
-        for r in 0..row_num {
-            // r 行の row を選ぶ
-            // println!("row_num: {}", row_num);
-            // println!("r: {}", r);
-            let combinations = (0..h).combinations_with_replacement(r);
-            // println!("{:?}", combinations);
-            for comb in combinations {
-                // 初期値
-                let mut updated_table = c.clone();
-
-                // 選ばれたところを塗り潰す
-                for i in comb {
-                    current_black -= updated_table[i].chars().filter(|&a| a == '#').count();
-                    // 既に要求数未満であれば `continue`
-                    if current_black < k {
-                        continue;
-                    }
-                    updated_table[i] = updated_table[i].replace('#', &"r");
+    for bit_row in 0..2u32.pow(h) {
+        // println!("bit_row: {}", bit_row);
+        let mut red_rows = vec!();
+        for i in 0..h {
+            if bit_row & (1 << i) > 0 {
+                red_rows.push(i);
+            }
+        }
+        // println!("red_rows: {:?}", red_rows);
+        for bit_col in 0..2u32.pow(w) {
+            // println!("bit_col: {}", bit_col);
+            let mut red_cols = vec!();
+            for i in 0..w {
+                if bit_col & (1 << i) > 0 {
+                    red_cols.push(i);
                 }
+            }
+            // println!("red_cols: {:?}", red_cols);
 
-                if current_black < k {
-                    continue;
-                }
-
-                current_table = updated_table;
-                // println!("before col_num");
-                // println!("current_black: {}", current_black);
-                for col_num in 0..w + 1 {
-                    for c in 0..col_num {
-                        let combinations = (0..w).combinations_with_replacement(c);
-                        for comb in combinations {
-                            // let mut updated_table = current_table.clone();
-
-                            for i in comb {
-                                let mut updated_black = current_black;
-                                for j in &current_table {
-                                    if j.as_bytes()[i] == b'#' {
-                                        println!("k: {}", k);
-                                        println!("updated_black: {}", updated_black);
-                                        updated_black -= 1;
-                                        if updated_black < k {
-                                            continue;
-                                        }
-                                    }
-                                    if updated_black < k {
-                                        continue;
-                                    }
-                                }
-                                current_black = updated_black;
-                            }
-
-                            if current_black == k {
-                                ans += 1;
+            let mut current_black = total_black;
+            'i_loop:
+            for i in 0..h {
+                for j in 0..w {
+                    if red_rows.contains(&i) || red_cols.contains(&j) {
+                        if c[i as usize][j as usize] == '#' {
+                            current_black -= 1;
+                            if current_black < k {
+                                break 'i_loop;
                             }
                         }
                     }
                 }
-                // println!("after col_num");
-                // println!("ans: {}", ans);
+            }
+            if current_black == k {
+                // println!("{}", current_black);
+                ans += 1;
             }
         }
     }
