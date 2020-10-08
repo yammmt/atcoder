8fca6ac12c6533eabe3369da4e1cc5c8c3c29ab1
yammmt <cu.cs.y20@gmail.com>
2020-10-08 21:10:13 +0900

    :dancer:

---
+++ abc/114/c/tests/sample_inputs.rs
@@ -0,0 +1,43 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./main";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"575
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "4\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"3600
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "13\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample3() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"999999999
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "26484\n");
+    assert!(output.stderr_str().is_empty());
+}
+
