3d5309cf39a4b9477fe2761afe79dc2ab994c125
yammmt <cu.cs.y20@gmail.com>
2020-10-27 20:44:56 +0900

    :punch:

--- abc/138/e/src/main.rs
+++ abc/138/e/src/main.rs
@@ -1,14 +1,25 @@
 // -*- coding:utf-8-unix -*-
 
-// WA
-// サンプルあるものの長すぎてデバッグ苦しい
-// 落ちたサンプルでデバッグビルド実行してもオーバーフロー/アンダーフロー (panic) は発生しない
-// AtCoder では isize は 64bit, 入力文字列も最高で 10^5 でありキャストバグることもないはず
-// まったくわからない
-
 // 愚直なシミュレーションは aaaa...z から zzz... を作ると TLE
 
 use proconio::input;
+
+// v[i] > k を満たす最小の i を返す
+// 見つからなければ `v.len()` を返す
+fn upper_bound(v: &[isize], k: isize) -> usize {
+    let mut low: isize = -1;
+    let mut high = v.len() as isize;
+
+    while high - low > 1 {
+        let mid = (low + high) / 2;
+        if v[mid as usize] > k {
+            high = mid;
+        } else {
+            low = mid;
+        }
+    }
+    high as usize
+}
 
 fn main() {
     input! {
@@ -19,10 +30,8 @@ fn main() {
     let vs = s.chars().collect::<Vec<char>>();
     let vt = t.chars().collect::<Vec<char>>();
     let mut vsappear = vec![vec![]; 26];
-    assert_eq!(s, vs.iter().collect::<String>());
     for i in 0..vs.len() {
-        // println!("[{}] char: {}, insert to: {}", i, vs[i], (vs[i] as u8 - b'a') as usize);
-        vsappear[(vs[i] as u8 - b'a') as usize].push(i);
+        vsappear[(vs[i] as u8 - b'a') as usize].push(i as isize);
     }
     for c in &vt {
         if vsappear[(*c as u8 - b'a') as usize].is_empty() {
@@ -32,58 +41,23 @@ fn main() {
         }
     }
 
-    // [a, z] の入力は拾えている
-    let mut total_len = 0;
-    for i in 0..26 {
-        total_len += vsappear[i].len();
-        // println!("{}: {:?}", i, vsappear[i]);
-    }
-    assert_eq!(total_len, vs.len());
-    // return;
-
     let mut ans = 0u64;
-    let mut vnextidx = vec![0usize; 26];
-    // ex. in/out: a/a
-    let mut last_idx = -1;
-    let mut resultstr = String::new();
+    let mut last_idx = -1isize;
     for c in &vt {
+        // println!("c: {}", c);
         let c_as_usize = (*c as u8 - b'a') as usize;
-        // println!("{}", c);
-        // println!("c_as_usize: {}", c_as_usize);
-        // vsappear[][] 最大値は 10^5 であり isize へのキャストは問題ない
-        if vsappear[c_as_usize][vnextidx[c_as_usize]] as isize > last_idx {
-            // ループする必要なし
-            // last_idx は初回に限り -1, 残りはすべて [0, 10^5 - 1] であり
-            // 上の条件と合わせて u64 へのキャストは失敗しない
-            ans += (vsappear[c_as_usize][vnextidx[c_as_usize]] as isize - last_idx) as u64;
-            // 配列要素外アクセスであれば RE になる
-            last_idx = vsappear[c_as_usize][vnextidx[c_as_usize]] as isize;
-            vnextidx[c_as_usize] = (vnextidx[c_as_usize] + 1) % vsappear[c_as_usize].len();
-            resultstr.push(*c);
-        } else {
-            // last_idx より後ろにその字が存在しないならループする
-            let mut is_loop = true;
-            // vnextidx[c] は vsappear[c][] のインデックスであり範囲は誤っていない
-            for i in vnextidx[c_as_usize]..vsappear[c_as_usize].len() {
-                if vsappear[c_as_usize][i] as isize > last_idx {
-                    is_loop = false;
-                    vnextidx[c_as_usize] = i;
-                    break;
-                }
-            }
-            if is_loop {
-                // 先頭に戻るまでに要した分を足しておく
-                // println!("vsappear[c_as_usize]: {:?}", vsappear[c_as_usize]);
-                ans += (vs.len() as isize - last_idx) as u64;
-                vnextidx[c_as_usize] = 0;
-                last_idx = 0;
-            }
-            ans += (vsappear[c_as_usize][vnextidx[c_as_usize]] as isize - last_idx) as u64;
-            last_idx = vsappear[c_as_usize][vnextidx[c_as_usize]] as isize;
-            vnextidx[c_as_usize] = (vnextidx[c_as_usize] + 1) % vsappear[c_as_usize].len();
-            resultstr.push(*c);
+        let mut cidx = upper_bound(&vsappear[c_as_usize][..], last_idx) as isize;
+        if cidx == vsappear[c_as_usize].len() as isize {
+            // loop
+            // println!("loop");
+            ans += (vs.len() as isize - last_idx) as u64;
+            cidx = 0;
+            last_idx = 0;
         }
+        ans += (vsappear[c_as_usize][cidx as usize] as isize - last_idx) as u64;
+        last_idx = vsappear[c_as_usize][cidx as usize] as isize;
+        // println!("ans: {}", ans);
     }
-    assert_eq!(resultstr, t);
+
     println!("{}", ans);
 }
--- abc_like/hhkb2020/e/src/main.rs
+++ abc_like/hhkb2020/e/src/main.rs
@@ -1,8 +1,8 @@
 // -*- coding:utf-8-unix -*-
 
-// WA
-// 2000x2000 全マス置ける場合に地球が爆発する計算量になるのではと思いきや TLE ではない
-// `2u64.pow()` がオーバーフローしたのだろう
+// https://drken1215.hatenablog.com/entry/2019/06/10/143300
+
+// 各マスに対する上下左右の範囲を毎度検索すると O(HHW + HWW) くらいになり TLE
 
 use proconio::input;
 
@@ -12,79 +12,90 @@ fn main() {
         w: usize,
     }
     let mut s = vec![];
-    let mut k = vec![];
-    for i in 0..h {
+    let mut empty_spaces = 0u64;
+    for _ in 0..h {
         input! {
             st: String,
         }
         let vc = st.chars().collect::<Vec<char>>();
-        for j in 0..vc.len() {
-            if vc[j] == '.' {
-                k.push((i as usize, j as usize));
-            }
-        }
+        empty_spaces += vc.iter().filter(|&c| *c == '.').count() as u64;
         s.push(vc);
     }
-    // println!("{:?}", k);
-    let divisor = 1_000_000_007;
-    let mut ans = 0u64;
-    for bit_row in 0..2u64.pow(k.len() as u32) {
-        let mut selected = vec![];
-        for i in 0..k.len() {
-            if bit_row & (1 << i) > 0 {
-                selected.push(i);
-            }
-        }
-        // println!("selected: {:?}", selected);
+    // println!("{:?}", s);
 
-        let mut mass = s.clone();
-        for i in &selected {
-            // println!("i: {:?}", i);
-            // println!("{:?}", k[*i as usize]);
-            // self
-            if mass[k[*i as usize].0][k[*i as usize].1] == '.' {
-                mass[k[*i as usize].0][k[*i as usize].1] = 'o';
-                ans = (ans + 1) % divisor;
+    let mut left = vec![vec![0u64; w]; h];
+    let mut right = vec![vec![0u64; w]; h];
+    let mut up = vec![vec![0u64; w]; h];
+    let mut down = vec![vec![0u64; w]; h];
+    for i in 0..h {
+        let mut cur = 0;
+        for j in 0..w {
+            if s[i][j] == '.' {
+                cur += 1;
+                left[i][j] = cur;
+            } else {
+                cur = 0;
             }
-            // h 負方向
-            for j in (0..k[*i as usize].0).rev() {
-                if mass[j][k[*i as usize].1] == '#' {
-                    break;
-                } else if mass[j][k[*i as usize].1] == '.' {
-                    mass[j][k[*i as usize].1] = 'o';
-                    ans = (ans + 1) % divisor;
-                }
-            }
-            // h 正方向
-            for j in k[*i as usize].0..h {
-                if mass[j][k[*i as usize].1] == '#' {
-                    break;
-                } else if mass[j][k[*i as usize].1] == '.' {
-                    mass[j][k[*i as usize].1] = 'o';
-                    ans = (ans + 1) % divisor;
-                }
-            }
-            // w 負方向
-            for j in (0..k[*i as usize].1).rev() {
-                if mass[k[*i as usize].0][j] == '#' {
-                    break;
-                } else if mass[k[*i as usize].0][j] == '.' {
-                    mass[k[*i as usize].0][j] = 'o';
-                    ans = (ans + 1) % divisor;
-                }
-            }
-            // w 正方向
-            for j in k[*i as usize].1..w {
-                if mass[k[*i as usize].0][j] == '#' {
-                    break;
-                } else if mass[k[*i as usize].0][j] == '.' {
-                    mass[k[*i as usize].0][j] = 'o';
-                    ans = (ans + 1) % divisor;
-                }
-            }
-            // println!("{:?}", mass);
         }
-        // println!("{:?}", mass);
+    }
+    for i in 0..h {
+        let mut cur = 0;
+        for j in (0..w).rev() {
+            if s[i][j] == '.' {
+                cur += 1;
+                right[i][j] = cur;
+            } else {
+                cur = 0;
+            }
+        }
+    }
+    for i in 0..w {
+        let mut cur = 0;
+        for j in 0..h {
+            if s[j][i] == '.' {
+                cur += 1;
+                down[j][i] = cur;
+            } else {
+                cur = 0;
+            }
+        }
+    }
+    for i in 0..w {
+        let mut cur = 0;
+        for j in (0..h).rev() {
+            if s[j][i] == '.' {
+                cur += 1;
+                up[j][i] = cur;
+            } else {
+                cur = 0;
+            }
+        }
+    }
+
+    let divisor = 1_000_000_007u64;
+    let mut twon = Vec::with_capacity(h * w + 1);
+    // 繰り返し二乗法だと高速だが h/w は高々 2,000 乗算なので間に合う
+    for i in 0..h * w + 1 {
+        if i == 0 {
+            twon.push(1);
+        } else {
+            twon.push((twon[i - 1] * 2) % divisor);
+        }
+    }
+
+    let mut ans = 0u64;
+    for i in 0..h {
+        for j in 0..w {
+            if s[i][j] == '#' {
+                continue;
+            }
+
+            // 自身を点灯する n 個のマスのうちどれかが点灯すれば良い
+            // 全消灯のパターンを引くための 2^n - 1
+            // 自身を点灯させない m 個のマスは点灯消灯どちらでも良いので単純に 2^m
+            let light_ptrn = left[i][j] + right[i][j] + up[i][j] + down[i][j] - 3;
+            ans = (ans + (((twon[light_ptrn as usize] + divisor - 1) % divisor) * twon[(empty_spaces - light_ptrn) as usize]) % divisor) % divisor;
+        }
     }
 
     println!("{}", ans);
