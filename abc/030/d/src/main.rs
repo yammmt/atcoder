afa98dac3ec56d26301f6a0d81320f921f752ccb
yammmt <cu.cs.y20@gmail.com>
2020-10-29 22:23:06 +0900

    :roll_eyes:

---
+++ abc/030/d/src/main.rs
@@ -0,0 +1,108 @@
+// -*- coding:utf-8-unix -*-
+
+// 別解: https://atcoder.jp/contests/abc030/submissions/17719897
+// i 回目の位置が bn[i] ではないことに気付かないとサンプルだけ通ってテストケース全滅した
+
+use proconio::input;
+use std::collections::HashMap;
+
+fn main() {
+    input! {
+        n: usize,
+        a: usize,
+        k: String,
+    }
+    let mut bn = vec![a];
+    for _ in 0..n {
+        input! {
+            b: usize,
+        }
+        bn.push(b);
+    }
+
+    let knum = k.parse::<u64>();
+    let is_k_small = match knum {
+        Ok(_) => true,
+        Err(_) => false,
+    };
+
+    let mut hm = HashMap::new();
+    let mut cycle = 0;
+    let mut offset = 0;
+    let mut place = vec![0; n + 1];
+    place[0] = a;
+    hm.insert(a, 0);
+    for i in 0..n + 1 {
+        // reach answer before loop
+        if is_k_small && Ok((i + 1) as u64) == knum {
+            println!("{}", bn[place[i]]);
+            return;
+        }
+
+        match hm.get(&bn[place[i]]) {
+            Some(v) => {
+                cycle = (i + 1 - v) as u64;
+                offset = *v as u64;
+                break;
+            },
+            None => {
+                hm.insert(bn[place[i]], i + 1);
+                place[i + 1] = bn[place[i]];
+            }
+        }
+    }
+    // println!("{:?}", hm);
+    // println!("place: {:?}", place);
+    // println!("cycle: {}", cycle);
+    // println!("offset: {}", offset);
+
+    // O(digit of k)
+    // k - offset
+    let mut coffset = offset;
+    let mut voffset = vec![];
+    while coffset > 0 {
+        voffset.push(coffset % 10);
+        coffset /= 10;
+    }
+    let mut vk = k.chars().map(|a| a.to_digit(10).unwrap() as u64).collect::<Vec<_>>();
+    for _ in 0..(vk.len() - voffset.len()) {
+        voffset.push(0);
+    }
+    voffset.reverse();
+    let mut is_carry = false;
+    for i in (0..voffset.len()).rev() {
+        if is_carry {
+            if vk[i] > 0 && vk[i] - 1 >= voffset[i] {
+                vk[i] = vk[i] - 1 - voffset[i];
+                is_carry = false;
+            } else {
+                vk[i] = 10 + vk[i] - 1 - voffset[i];
+            }
+        } else {
+            if vk[i] >= voffset[i] {
+                vk[i] = vk[i] - voffset[i];
+            } else {
+                vk[i] = 10 + vk[i] - voffset[i];
+                is_carry = true;
+            }
+        }
+    }
+    // println!("{} - {:?} = {:?}", k, voffset, vk);
+
+    // (k - offset) % cycle
+    let mut cnum = 0;
+    for i in 0..vk.len() {
+        cnum = cnum * 10 + vk[i];
+        if cnum < cycle {
+            continue;
+        }
+
+        cnum = cnum % cycle;
+    }
+    // println!("cycle: {}", cycle);
+    // println!("offset: {}", offset);
+    // println!("cnum: {}", cnum);
+
+    // (k - offset) % cycle 分進んで ans
+    println!("{}", place[(offset + cnum) as usize]);
+}
---
+++ abc/030/d/tests/sample_inputs.rs
@@ -0,0 +1,63 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./main";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"6 4
+5
+2 3 1 2 6 5
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "3\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"4 1
+100000000000000000000
+2 3 4 1
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "1\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample3() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"8 1
+1
+2 3 4 5 3 2 4 5
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "2\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample4() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"4 1
+5
+2 3 4 1
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "2\n");
+    assert!(output.stderr_str().is_empty());
+}
