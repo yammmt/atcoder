b2ccebc338d5d7129ae31e23c7875890800b88e8
yammmt <cu.cs.y20@gmail.com>
2020-12-06 21:17:07 +0900

    :zzz: :zzz:

--- abc/abc085/src/bin/c.rs
+++ abc/abc085/src/bin/c.rs
@@ -2,5 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        y: u64,
     }
+
+    for a in 0..n + 1 {
+        for b in 0..n + 1 {
+            if a + b > n {
+                break;
+            }
+
+            let c = n - a - b;
+            if (a * 10000 + b * 5000 + c * 1000) as u64 == y {
+                println!("{} {} {}", a, b, c);
+                return;
+            }
+        }
+    }
+    println!("-1 -1 -1");
 }
