c183e495ff7723e1545dd262ee0e60bbc65dc537
yammmt <cu.cs.y20@gmail.com>
2021-03-17 21:06:29 +0900

    :lock_with_ink_pen:

--- abc/175/d/src/main.rs
+++ abc/175/d/src/main.rs
@@ -1,6 +1,5 @@
-// -*- coding:utf-8-unix -*-
-
-// TLE
+// 49min 1WA (33min)
+// WA: ループ端数を増やし忘れ これは引っ掛かる
 
 use proconio::input;
 
@@ -8,21 +7,43 @@ fn main() {
     input! {
         n: usize,
         k: i64,
-        p: [i64; n],
-        c: [i64; n],
+        pn: [usize; n],
+        cn: [i64; n],
     }
 
-    let mut ans: i64 = std::i64::MIN;
+    // 自身のマスから始めて一サイクルでどれだけ増えるか/最大範囲はどこか
+    // 一サイクルが正 => 回せるだけ回って残りは最大値になる範囲まで動く
+    // 一サイクルが負 => 負の中で最大となる範囲まで一度だけ動く
+    let mut ans = std::i64::MIN / 2;
     for i in 0..n {
-        let mut next_mas = p[i] - 1;
-        let mut current_pts = 0;
-        for _j in 0..k {
-            current_pts += c[next_mas as usize];
-            next_mas = p[next_mas as usize] - 1;
-            if current_pts > ans {
-                ans = current_pts;
-            }
+        // println!("i: {}", i);
+        // i 始点
+        let mut cur_i = i;
+        let mut cycle_len = 0;
+        let mut pts_total = 0;
+        while cycle_len < k && (cycle_len < 1 || cur_i != i) {
+            cur_i = pn[cur_i] - 1;
+            pts_total += cn[cur_i];
+            ans = ans.max(pts_total);
+            cycle_len += 1;
         }
+        // println!("{}", pts_total);
+
+        if cycle_len < k && pts_total > 0 {
+            // 回せるだけループ
+            // 一周手前から見ないと -5 => 7 => -1 のようなループで WA しそう
+            let mut cur_i = i;
+            let loop_num = k / cycle_len - 1;
+            let mut pts_cur = pts_total * loop_num;
+            ans = ans.max(pts_cur);
+            for _ in 0..k % cycle_len + cycle_len {
+                cur_i = pn[cur_i] - 1;
+                pts_cur += cn[cur_i];
+                ans = ans.max(pts_cur);
+            }
+            // println!("  {}", pts_cur);
+        }
+        // println!("{}", ans);
     }
     println!("{}", ans);
 }
