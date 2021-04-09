cbd2e0a8d9da1b093937e4be03448f916a72ac92
yammmt <cu.cs.y20@gmail.com>
2021-04-09 22:05:59 +0900

    :angel:

--- abc/abc027/src/bin/c.rs
+++ abc/abc027/src/bin/c.rs
@@ -1,11 +1,30 @@
+// :fu: 21-04 ゲーム
+// https://qiita.com/hamamu/items/55433210be3c47a4dd72
+
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use permutohedron::heap_recursive;
 
 fn main() {
     input! {
+        mut n: u64,
     }
+
+    let mut tkhs_win = true;
+    let mut lr = (n + 1, 2 * n + 1);
+    while lr.0 != 1 {
+        lr = if tkhs_win {
+            ((lr.0 + 1) / 2, lr.0 - 1)
+        } else {
+            (lr.0 / 2, lr.0 - 1)
+        };
+        tkhs_win = !tkhs_win;
+    }
+
+    println!(
+        "{}",
+        if tkhs_win {
+            "Takahashi"
+        } else {
+            "Aoki"
+        }
+    );
 }
