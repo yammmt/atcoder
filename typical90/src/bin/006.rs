24fe9eddbdb012eb45214e0f15123bfcdbcdd04f
yammmt <cu.cs.y20@gmail.com>
2021-04-23 20:04:54 +0900

    :it:

--- typical90/src/bin/006.rs
+++ typical90/src/bin/006.rs
@@ -1,12 +1,42 @@
-// use petgraph::unionfind::UnionFind;
+// :fu: 21-04 遅い
+
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use permutohedron::heap_recursive;
+use proconio::marker::Chars;
+use std::collections::VecDeque;
 
 fn main() {
     input! {
+        n: usize,
+        k: usize,
+        s: Chars,
     }
+
+    let mut abc = vec![VecDeque::new(); 26];
+    for (i, c) in s.iter().enumerate() {
+        abc[(*c as u8 - b'a') as usize].push_back(i);
+    }
+
+    let mut ans = vec![];
+    let mut last_i = -1;
+    for i in 0..k {
+        // println!("i: {}", i);
+        // println!("{:?}", abc);
+        let available_i = n - k + i;
+        'j_loop: for j in 0..26 {
+            // println!("  {}", j);
+            while let Some(cur_i) = abc[j].pop_front() {
+                if cur_i as isize > last_i && cur_i <= available_i {
+                    // println!("  {}", (b'a' + j as u8) as char);
+                    ans.push((b'a' + j as u8) as char);
+                    last_i = cur_i as isize;
+                    break 'j_loop;
+                } else if cur_i > available_i {
+                    abc[j].push_front(cur_i);
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans.iter().collect::<String>());
 }
--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -1,12 +1,42 @@
-// use petgraph::unionfind::UnionFind;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use permutohedron::heap_recursive;
+
+// v[i] >= k を満たす最小の i を返す (or k 以下である v の要素数)
+// 見つからなければ `v.len()` を返す
+// `v` はソートされていること
+fn lower_bound(v: &[i32], k: i32) -> usize {
+    let mut low: isize = -1;
+    let mut high = v.len() as isize;
+
+    while high - low > 1 {
+        let mid = (low + high) / 2;
+        if v[mid as usize] >= k {
+            high = mid;
+        } else {
+            low = mid;
+        }
+    }
+    high as usize
+}
 
 fn main() {
     input! {
+        n: usize,
+        mut an: [i32; n],
+        q: usize,
+        bq: [i32; q],
+    }
+    an.sort();
+    for b in &bq {
+        let lb = lower_bound(&an, *b);
+        println!(
+            "{}",
+            if lb == 0 {
+                an[0] - *b
+            } else if lb == n {
+                *b - an[n - 1]
+            } else {
+                (an[lb] - *b).min(*b - an[lb - 1])
+            }
+        );
     }
 }
