fe5a6ef236622191829117fefcd099c6fc70645f
yammmt <cu.cs.y20@gmail.com>
2021-05-12 22:03:16 +0900

    :x:

--- typical90/src/bin/032.rs
+++ typical90/src/bin/032.rs
@@ -1,11 +1,42 @@
-// use petgraph::unionfind::UnionFind;
+use itertools::Itertools;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use permutohedron::heap_recursive;
+use std::collections::HashSet;
+
+static DUMMY: usize = std::usize::MAX / 4;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        ann: [[usize; n]; n],
+        m: usize,
+        xym: [(usize, usize); m],
+    }
+
+    let mut banned = HashSet::new();
+    for xy in &xym {
+        banned.insert((xy.0 - 1, xy.1 - 1));
+        banned.insert((xy.1 - 1, xy.0 - 1));
+    }
+
+    let mut ans = DUMMY;
+    for perm in (0..n).permutations(n) {
+        let mut cur = 0;
+        for i in 0..n {
+            cur += ann[perm[i]][i];
+            if i > 0 && banned.contains(&(perm[i], perm[i - 1])) {
+                cur = DUMMY;
+                break;
+            }
+        }
+        ans = ans.min(cur);
+    }
+
+    println!(
+        "{}",
+        if ans == DUMMY {
+            -1
+        } else {
+            ans as isize
+        }
+    );
 }
