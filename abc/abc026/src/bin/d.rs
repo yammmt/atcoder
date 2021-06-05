a18a88d39bb06b99a203e6cfec6a628653f597f3
yammmt <cu.cs.y20@gmail.com>
2021-06-05 23:22:59 +0900

    :id:

--- abc/abc026/src/bin/d.rs
+++ abc/abc026/src/bin/d.rs
@@ -1,11 +1,23 @@
+// :fu: 21-06 数問風
+
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use permutohedron::heap_recursive;
 
 fn main() {
     input! {
+        a: f64,
+        b: f64,
+        c: f64,
     }
+
+    let mut l = 0.0;
+    let mut h = 200.0;
+    while h - l > 1e-12 {
+        let m = (h + l) / 2.0;
+        if a * m + b * (c * m * std::f64::consts::PI).sin() < 100.0 {
+            l = m;
+        } else {
+            h = m;
+        }
+    }
+    println!("{}", l);
 }
--- abc/abc105/src/bin/d.rs
+++ abc/abc105/src/bin/d.rs
@@ -1,11 +1,29 @@
+// :fu: 21-06 単純にする
+// コードテストで通るサンプル 2 がジャッジに通らない なぜ？
+
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use permutohedron::heap_recursive;
+use std::collections::HashMap;
 
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+        mut an: [usize; n],
     }
+    an.iter_mut().for_each(|a| *a %= m);
+    // println!("{:?}", an);
+
+    let mut ans = 0usize;
+    let mut hm = HashMap::new();
+    hm.insert(0, 1);
+    let mut cur = 0;
+    for a in &an {
+        cur = (cur + *a) % m;
+        let cnt = hm.entry(cur).or_insert(0);
+        ans += *cnt;
+        *cnt += 1;
+    }
+    // println!("{:?}", hm);
+
+    println!("{}", ans);
 }
