1180cd596a5003bcb3dd2275ab99ff904bbd8490
yammmt <cu.cs.y20@gmail.com>
2021-08-10 21:15:06 +0900

    :pray:

--- abc/114/c/src/main.rs
+++ abc/114/c/src/main.rs
@@ -1,68 +1,22 @@
-// -*- coding:utf-8-unix -*-
-
 use proconio::input;
-use permutohedron::heap_recursive;
-use std::collections::HashSet;
 
-// Debug ビルド (通常の `cargo test`) と Release ビルドでかなりの速度差がある
-
-fn num_of_digit(mut n: usize) -> usize {
-    let mut ans = 0;
-    while n != 0 {
-        n /= 10;
-        ans += 1;
-    }
-    ans
-}
-
-fn solve(n: usize, v: &Vec<u64>, mut hs_v: &mut HashSet<Vec<u64>>) -> usize {
-    let mut data = v.clone();
-    // `HashSet` の重複回避
-    // data.len() <= 9 であり十分高速
-    data.sort();
-    if hs_v.contains(&data) {
+fn dfs(cur: u64, n: u64, uses: (bool, bool, bool)) -> usize {
+    if cur > n {
         return 0;
     }
 
-    hs_v.insert(data.clone());
-    let mut ans = 0;
-    let mut num_p = 0;
-    let mut hs_num = HashSet::new();
-    heap_recursive(&mut data, |p| {
-        let mut current = 0;
-        for i in 0..v.len() {
-            current *= 10;
-            current += p[i];
-        }
-        // 重複する組み合わせを回避
-        if !hs_num.contains(&current) {
-            hs_num.insert(current);
-            if current as usize <= n {
-                // println!("{}", current);
-                ans += 1;
-            }
-            num_p += 1;
-        }
-    });
-    // 桁数基準で再帰止めてやらないと呼び出し回数が三度も多くなる (それが何倍にもなる)
-    if ans != 0 && ans == num_p && num_of_digit(n) > data.len() {
-        let mut v3 = v.clone();
-        v3.push(3);
-        let mut v5 = v.clone();
-        v5.push(5);
-        let mut v7 = v.clone();
-        v7.push(7);
-        ans + solve(n, &v3, &mut hs_v) + solve(n, &v5, &mut hs_v) + solve(n, &v7, &mut hs_v)
-    } else {
-        ans
-    }
+    let mut ret = if uses == (true, true, true) { 1 } else { 0 };
+    ret += dfs(cur * 10 + 3, n, (true, uses.1, uses.2));
+    ret += dfs(cur * 10 + 5, n, (uses.0, true, uses.2));
+    ret += dfs(cur * 10 + 7, n, (uses.0, uses.1, true));
+    ret
 }
 
 fn main() {
     input! {
-        n: usize,
+        n: u64,
     }
 
-    let base = vec![3, 5, 7];
-    println!("{}", solve(n, &base, &mut HashSet::new()));
+    let ans = dfs(0, n, (false, false, false));
+    println!("{}", ans);
 }
