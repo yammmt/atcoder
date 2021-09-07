6ac65e2751f767a6211b3616248f32484a7b79e3
yammmt <cu.cs.y20@gmail.com>
2021-09-07 21:16:44 +0900

    :thumbsup:

--- abc/abc215/src/bin/e.rs
+++ abc/abc215/src/bin/e.rs
@@ -1,12 +1,45 @@
-// :fu: 21-08 わからん
+// :fu: 21-08 bitDP つらい
 
 use proconio::input;
-use proconio::marker::Chars;
+use proconio::marker::Bytes;
+
+const BIT_LEN_MAX: usize = 1 << 10;
 
 fn main() {
     input! {
-        n: usize,
-        s: Chars,
+        _n: usize,
+        s: Bytes,
     }
     let d = 998_244_353;
+
+    // dp[i][j]: 出場したコンテストの集合が i であって最後に出場したものが j
+    let mut dp = vec![vec![0; 11]; BIT_LEN_MAX];
+    dp[0][0] = 1;
+    for c in &s {
+        let cur_byte = (*c - b'A' + 1) as usize;
+        let mut cur = vec![vec![0; 11]; BIT_LEN_MAX];
+        for i in 0..BIT_LEN_MAX {
+            for j in 0..11 {
+                if dp[i][j] == 0 {
+                    continue;
+                }
+
+                cur[i][j] = (cur[i][j] + dp[i][j]) % d;
+
+                // 最後に出たものと同じ or まだ出ていないなら付け足しても良い
+                if cur_byte == j || i & (1 << (cur_byte - 1)) == 0 {
+                    let next_i = i | (1 << (cur_byte - 1));
+                    cur[next_i][cur_byte] = (cur[next_i][cur_byte] + dp[i][j]) % d;
+                }
+            }
+        }
+
+        dp = cur;
+    }
+
+    // "1 個以上選ぶ"
+    dp[0][0] = 0;
+    let mut ans = 0u64;
+    dp.iter().for_each(|v| ans = (ans + v.iter().sum::<u64>()) % d);
+    println!("{}", ans);
 }
--- abc/abc216/src/bin/f.rs
+++ abc/abc216/src/bin/f.rs
@@ -1,16 +1,50 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
 
-// static DUMMY: usize = std::usize::MAX / 4;
+const DP_LEN: usize = 5001;
+// const DP_LEN: usize = 10;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        an: [usize; n],
+        bn: [usize; n],
+    }
+    let d = 998_244_353;
+    let mut abn = (0..n)
+        .map(|i| (an[i], bn[i]))
+        .collect::<Vec<(usize, usize)>>();
+    abn.sort_unstable();
+
+    let mut ans = 0;
+    // dp[i]: B の和が i となる数の個数
+    let mut dp = vec![0; DP_LEN];
+    dp[0] = 1;
+    for ab in &abn {
+        let mut cur = vec![0; DP_LEN];
+        // 今見ている ab は必ず選ぶとする
+        for (i, c) in dp.iter().enumerate() {
+            // println!("i: {}", i);
+            // println!("c: {}", c);
+            if *c == 0 {
+                continue;
+            }
+
+            cur[i] = (cur[i] + *c) % d;
+
+            let next = i + ab.1;
+            // println!("  next: {}", next);
+            if next < DP_LEN {
+                cur[next] = (cur[next] + *c) % d;
+            }
+            if next <= ab.0 {
+                // println!("  ans++");
+                ans = (ans + *c) % d;
+            }
+        }
+        // println!("{:?}", cur);
+        // println!("  {}", ans);
+        dp = cur;
+    }
+
+    println!("{}", ans);
 }
