51ac93419863e5efea98d282674e17bf77b46b98
yammmt <cu.cs.y20@gmail.com>
2022-01-23 17:14:31 +0900

    :x: :x: :x: :x:

--- abc/abc233/src/bin/e.rs
+++ abc/abc233/src/bin/e.rs
@@ -1,18 +1,37 @@
-// TODO:
+// 数問 嫌
 
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-
-// static DUMMY: usize = std::usize::MAX / 4;
+use proconio::marker::Bytes;
 
 fn main() {
-    input! {}
+    input! {
+        x: Bytes,
+    }
+    let mut vn: Vec<usize> = x.iter().map(|n| (n - b'0') as usize).collect();
+    vn.reverse();
+    let vn = vn;
+
+    // 愚直に文字列で繰り上がり含め計算すると
+    // 最大入力が 5x10^5 であり桁数二乗のオーダーでは TLE
+    let mut cusum: usize = vn.iter().sum();
+    let mut carry_up = 0;
+    let mut ans = vec![];
+    for n in &vn {
+        let cur = cusum + carry_up;
+        ans.push(cur % 10);
+        // println!("ans: {:?}", ans);
+        // println!("  cusum: {}", cusum);
+        carry_up = cur / 10;
+        cusum -= *n;
+    }
+    while carry_up > 0 {
+        ans.push(carry_up % 10);
+        carry_up /= 10;
+    }
+
+    ans.reverse();
+    for a in ans {
+        print!("{}", a);
+    }
+    println!();
 }
--- abc/abc234/src/bin/e.rs
+++ abc/abc234/src/bin/e.rs
@@ -1,16 +1,52 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
-use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
+// 32min 数問？
 
-// static DUMMY: usize = std::usize::MAX / 4;
+use proconio::input;
+
+fn to_num(v: Vec<i64>) -> i64 {
+    let mut ret = 0;
+    for vv in &v {
+        ret *= 10;
+        ret += *vv;
+    }
+    ret
+}
 
 fn main() {
-    input! {}
+    input! {
+        x: i64,
+    }
+
+    let mut ans = std::i64::MAX;
+    // 最下位桁を a とする
+    for a in 0..10 {
+        // println!("a: {}", a);
+        // 公差を b とする
+        for b in -9..10 {
+            // println!("  b: {}", b);
+            if a == 0 && b == 0 {
+                continue;
+            }
+
+            let mut vcur = vec![a];
+            loop {
+                let mut vcur_rev = vcur.clone();
+                vcur_rev.reverse();
+                let cur_num = to_num(vcur_rev);
+                if cur_num >= x {
+                    // println!("    {}", cur_num);
+                    ans = ans.min(cur_num);
+                    break;
+                }
+
+                let next = *vcur.last().unwrap() + b;
+                if !(0..10).contains(&next) {
+                    break;
+                }
+
+                vcur.push(next);
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
