512abdaff1644ae817096342547933fc6ae0c5a3
yammmt <cu.cs.y20@gmail.com>
2022-01-25 20:34:39 +0900

    :yawning_face:

--- abc/abc088/src/bin/d.rs
+++ abc/abc088/src/bin/d.rs
@@ -1,50 +1,50 @@
-// 14.5min
+// 14.5min (2020-11) -> 10min(2022-1)
 
 use proconio::input;
 use proconio::marker::Chars;
 use std::collections::VecDeque;
 
+const DUMMY: usize = std::usize::MAX / 2;
+
 fn main() {
     input! {
         h: usize,
         w: usize,
-        s: [Chars; h],
+        shw: [Chars; h],
     }
+    let dir = [(-1, 0), (1, 0), (0, -1), (0, 1)];
 
-    let mut counts = vec![vec![std::usize::MAX; w]; h];
-    counts[0][0] = 1;
+    // 通った白マスの数 (既に踏んだ白マスの数)
+    let mut scores = vec![vec![DUMMY; w]; h];
     let mut vdq = VecDeque::new();
     vdq.push_back((0, 0));
-    while !vdq.is_empty() {
-        let cur = vdq.pop_front().unwrap();
-        if cur.0 > 0 && s[cur.0 - 1][cur.1] == '.' && counts[cur.0 - 1][cur.1] == std::usize::MAX {
-            vdq.push_back((cur.0 - 1, cur.1));
-            counts[cur.0 - 1][cur.1] = counts[cur.0][cur.1] + 1;
-        }
-        if cur.0 < h - 1 && s[cur.0 + 1][cur.1] == '.' && counts[cur.0 + 1][cur.1] == std::usize::MAX {
-            vdq.push_back((cur.0 + 1, cur.1));
-            counts[cur.0 + 1][cur.1] = counts[cur.0][cur.1] + 1;
-        }
-        if cur.1 > 0 && s[cur.0][cur.1 - 1] == '.' && counts[cur.0][cur.1 - 1] == std::usize::MAX {
-            vdq.push_back((cur.0, cur.1 - 1));
-            counts[cur.0][cur.1 - 1] = counts[cur.0][cur.1] + 1;
-        }
-        if cur.1 < w - 1 && s[cur.0][cur.1 + 1] == '.' && counts[cur.0][cur.1 + 1] == std::usize::MAX {
-            vdq.push_back((cur.0, cur.1 + 1));
-            counts[cur.0][cur.1 + 1] = counts[cur.0][cur.1] + 1;
+    scores[0][0] = 1;
+    while let Some(cur) = vdq.pop_front() {
+        let nscore = scores[cur.0][cur.1] + 1;
+        for d in &dir {
+            let nh_i = cur.0 as isize + d.0;
+            let nw_i = cur.1 as isize + d.1;
+            if nh_i < 0 || nh_i >= h as isize || nw_i < 0 || nw_i >= w as isize {
+                continue;
+            }
+
+            let nh_u = nh_i as usize;
+            let nw_u = nw_i as usize;
+            if nscore < scores[nh_u][nw_u] && shw[nh_u][nw_u] == '.' {
+                vdq.push_back((nh_u, nw_u));
+                scores[nh_u][nw_u] = nscore;
+            }
         }
     }
 
-    if counts[h - 1][w - 1] == std::usize::MAX {
+    if scores[h - 1][w - 1] == DUMMY {
         println!("-1");
         return;
     }
 
-    let default_black_num = s.iter().fold(0, |su, row| {
-        su + row.iter().filter(|&&c| c == '#').count()
+    let white_num = shw.iter().fold(0, |su, row| {
+        su + row.iter().filter(|&&c| c == '.').count()
     });
-    println!(
-        "{}",
-        h * w - default_black_num - counts[h - 1][w - 1]
-    );
+
+    println!("{}", white_num - scores[h - 1][w - 1]);
 }
--- abc/abc213/src/bin/e.rs
+++ abc/abc213/src/bin/e.rs
@@ -1,3 +1,5 @@
+// 35 (21-08) -> 25 (22-01) 実装が下手
+
 use proconio::input;
 use proconio::marker::Chars;
 use std::collections::VecDeque;
@@ -11,69 +13,67 @@ fn main() {
         shw: [Chars; h],
     }
     let dir = [(-1, 0), (1, 0), (0, -1), (0, 1)];
-    let crash_dir = [-1, 0, 1];
 
-    // 破壊範囲が一マスであれば 0-1 BFS 典型
-    // 愚直にマス目の状態を渡すと O(HHWW) となり TLE
-    let mut visited = vec![vec![false; w]; h];
-    let mut dp = vec![vec![DUMMY; w]; h];
+    // パンチを一回繰り出すと最大 4 つのマスにスコアが入る
+    let mut scores = vec![vec![DUMMY; w]; h];
     let mut vdq = VecDeque::new();
+    // ((現在地), パンチ回数)
     vdq.push_back(((0, 0), 0));
+    // スコアも先に更新しておく
+    scores[0][0] = 0;
     while let Some(cur) = vdq.pop_front() {
-        if visited[(cur.0).0][(cur.0).1] {
+        if cur.1 > scores[(cur.0).0][(cur.0).1] {
             continue;
         }
 
-        // println!("{:?}", cur.0);
-        visited[(cur.0).0][(cur.0).1] = true;
-        dp[(cur.0).0][(cur.0).1] = cur.1;
         for d in &dir {
-            let h_i = (cur.0).0 as isize + d.0;
-            let w_i = (cur.0).1 as isize + d.1;
-            if h_i < 0 || w_i < 0 || h_i > h as isize - 1 || w_i > w as isize - 1 {
+            let nh_i = (cur.0).0 as isize + d.0;
+            let nw_i = (cur.0).1 as isize + d.1;
+            if nh_i < 0 || nh_i >= h as isize || nw_i < 0 || nw_i >= w as isize {
                 continue;
             }
 
-            let h_u = h_i as usize;
-            let w_u = w_i as usize;
-            // 多分冗長
-            if visited[h_u][w_u] || cur.1 >= dp[h_u][w_u] {
+            let nh_u = nh_i as usize;
+            let nw_u = nw_i as usize;
+            if cur.1 >= scores[nh_u][nw_u] {
                 continue;
             }
 
-            if shw[h_u][w_u] == '.' {
-                vdq.push_front(((h_u, w_u), cur.1));
+            if shw[nh_u][nw_u] == '.' {
+                vdq.push_front(((nh_u, nw_u), cur.1));
+                scores[nh_u][nw_u] = cur.1;
             } else {
-                vdq.push_back(((h_u, w_u), cur.1 + 1));
-                for hd in &crash_dir {
-                    for wd in &crash_dir {
-                        if *hd == 0 && *wd == 0 {
-                            continue;
-                        }
+                // パンチ時には (-1, -1) と (1, 1) も判定する
+                // (-2..=2) で端だけ弾いたほうが良いし, マスの状態によらずパンチした方がもっと楽
+                let punch_dir = match d {
+                    (-1, 0) => [(-2, -1), (-2, 0), (-2, 1), (-1, -1), (-1, 0), (-1, 1)],
+                    (1, 0) => [(2, -1), (2, 0), (2, 1), (1, -1), (1, 0), (1, 1)],
+                    (0, -1) => [(-1, -2), (-1, -1), (0, -2), (0, -1), (1, -2), (1, -1)],
+                    (0, 1) => [(-1, 2), (-1, 1), (0, 2), (0, 1), (1, 2), (1, 1)],
+                    _ => unreachable!(),
+                };
 
-                        let c_h_i = h_i + *hd;
-                        let c_w_i = w_i + *wd;
-                        if c_h_i < 0
-                            || c_w_i < 0
-                            || c_h_i > h as isize - 1
-                            || c_w_i > w as isize - 1
-                        {
-                            continue;
-                        }
-
-                        let c_h_u = c_h_i as usize;
-                        let c_w_u = c_w_i as usize;
-                        if visited[c_h_u][c_w_u] {
-                            continue;
-                        }
-
-                        vdq.push_back(((c_h_u, c_w_u), cur.1 + 1));
+                for pd in &punch_dir {
+                    let nh_i = (cur.0).0 as isize + pd.0;
+                    let nw_i = (cur.0).1 as isize + pd.1;
+                    if nh_i < 0 || nh_i >= h as isize || nw_i < 0 || nw_i >= w as isize {
+                        continue;
                     }
+
+                    let ns = cur.1 + 1;
+                    let nh_u = nh_i as usize;
+                    let nw_u = nw_i as usize;
+                    if ns >= scores[nh_u][nw_u] {
+                        continue;
+                    }
+
+                    vdq.push_back(((nh_u, nw_u), ns));
+                    scores[nh_u][nw_u] = ns;
                 }
             }
         }
     }
-    // println!("{:?}", dp);
+    // println!("{:?}", scores);
 
-    println!("{}", dp[h - 1][w - 1]);
+    println!("{}", scores[h - 1][w - 1]);
 }
