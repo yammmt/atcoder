50f95ccf4c559fe653e0139352e07dc3941d8f7a
yammmt <cu.cs.y20@gmail.com>
2022-02-18 22:41:02 +0900

    :face_in_clouds:

--- abc/abc237/src/bin/e.rs
+++ abc/abc237/src/bin/e.rs
@@ -1,7 +1,11 @@
-// :fu: 22-02 インフレが激しい
+// :fu: 22-02 嘘解が通っていたらしいがどれほどかは知らない
+// テストケースはしばらくあがっていない なんとかして
+// https://qiita.com/u2dayo/items/3f1ea15ec3b71d021b43#e%E5%95%8F%E9%A1%8Cskiing
 
 use proconio::input;
 use std::collections::BinaryHeap;
+
+const DUMMY: isize = std::isize::MIN / 4;
 
 fn main() {
     input! {
@@ -19,5 +23,46 @@ fn main() {
     let edges = edges;
 
     // 登る高さを最小化したいが Dijkstra は負辺を扱えない
-    // ポテンシャルというらしい 知らな
+    // ポテンシャルというらしい 初遭遇？
+
+    // 坂を下るぶんには "高さ + 楽しさ" は変化しない
+    // FIXME: なんもわからん
+    // 最終的な答えが "scores - 自身の高さ" であることから逆算するとこうだが
+    // 始点の score を 0 にして高い方向に進む場合はそのぶん不方向に動かして最後に辻褄合わせた方が自然？
+    let mut heap = BinaryHeap::new();
+    let mut scores = vec![DUMMY; n];
+    // (到達コスト, 頂点)
+    heap.push((hn[0], 0));
+    while let Some(cur) = heap.pop() {
+        // println!("cur: {:?}", cur);
+        // println!("  scores: {:?}", scores);
+        // assert!(cur.0 <= 0);
+        if scores[cur.1] != DUMMY {
+            continue;
+        }
+
+        scores[cur.1] = cur.0;
+
+        for v in &edges[cur.1] {
+            if scores[*v] != DUMMY {
+                continue;
+            }
+
+            heap.push(
+                if hn[*v] <= hn[cur.1] {
+                    (cur.0, *v)
+                } else {
+                    (cur.0 - hn[*v] + hn[cur.1], *v)
+                }
+            );
+        }
+    }
+    // println!("{:?}", scores);
+
+    // 最終的な到達点から (下りきれなかったぶんの) 高さを引いたものが答えになる
+    let mut ans = DUMMY;
+    for i in 0..n {
+        ans = ans.max(scores[i] - hn[i]);
+    }
+    println!("{}", ans);
 }
