82955bdcf6f4ef3948368eb3e0deaefa69e415a0
yammmt <cu.cs.y20@gmail.com>
2023-10-27 22:48:52 +0900

    :disguised_face:

--- v1.70.0/abc/abc245/src/bin/d.rs
+++ v1.70.0/abc/abc245/src/bin/d.rs
@@ -1,16 +1,43 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
-use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
+// とてもつらい
 
-// static DUMMY: usize = std::usize::MAX / 4;
+use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize,
+        an: [i64; n + 1],
+        cnm: [i64; n + m + 1],
+    }
+
+    let mut bm = vec![0; m + 1];
+    for i in (0..=m).rev() {
+        // an[n] で割る行為と対応が取れる範囲
+        // println!("i: {i}");
+        let c_idx = i + n;
+        // println!("  c_idx: {c_idx}");
+        let mut rhv = 0;
+        // a, b の添字 i, j の和が c の添字に等しく, かつ j が今求めようとしている値では
+        // ないこと
+        // j 始点: bm[i] より大きい添字の値はここまでに求まっているので使える
+        // 上限が m: bm の最大要素に到達
+        // 上限が c_idx: c の次数が m より小さいため打ち切る
+        for j in i + 1..=m.min(c_idx) {
+            let k = c_idx - j;
+            // println!("  j: {j}, k: {k}");
+            rhv += bm[j] * an[k];
+        }
+
+        // println!("  rhv: {rhv}");
+        bm[i] = (cnm[c_idx] - rhv) / an[n];
+        // println!("  bm[i]: {}", bm[i]);
+    }
+
+    for (i, b) in bm.iter().enumerate() {
+        if i == m {
+            println!("{b}");
+        } else {
+            print!("{b} ");
+        }
+    }
 }
--- v1.70.0/past/past202109-open/src/bin/i.rs
+++ v1.70.0/past/past202109-open/src/bin/i.rs
@@ -1,16 +1,35 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
-use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
+use proconio::{fastout, input};
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 
-// static DUMMY: usize = std::usize::MAX / 4;
-
+#[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        an: [u64; n],
+    }
+
+    // 最小値として達成可能な最大値, 作れる値について二分探索？
+    // pq を用意して全部入れる, 値を取り出して 2 で割る, pq から最小値を取りだして x3?
+    // 2 で割るのチェックで奇数を引き続けると辛いので pq は偶奇で２つ
+    // 最悪ケースでは各項について 32 回ずつ 2 で割れる可能性があり 3x10^6 回ほど pq 操作
+    // 10^8 は切れそう
+
+    let mut available_x3 = 0;
+    let mut odds = BinaryHeap::new();
+    for mut a in an {
+        while a % 2 == 0 {
+            a /= 2;
+            available_x3 += 1;
+        }
+        odds.push(Reverse(a));
+    }
+
+    for _ in 0..available_x3 {
+        let Reverse(o) = unsafe { odds.pop().unwrap_unchecked() };
+        odds.push(Reverse(3 * o));
+    }
+
+    let Reverse(ans) = unsafe { odds.pop().unwrap_unchecked() };
+    println!("{ans}");
 }
