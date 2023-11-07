8b31e216b90699260a0ce4b491aecd40271c9852
yammmt <cu.cs.y20@gmail.com>
2023-11-07 21:41:01 +0900

    :nerd_face:

--- v1.70.0/abc/abc322/src/bin/d.rs
+++ v1.70.0/abc/abc322/src/bin/d.rs
@@ -1,17 +1,140 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
-use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
+// TODO: マスを座標集合でもつ方針を試す
 
-// static DUMMY: usize = std::usize::MAX / 4;
+use proconio::input;
+use proconio::marker::Chars;
+
+fn turn_left(sin: &Vec<Vec<char>>, n: usize) -> Vec<Vec<char>> {
+    let mut ans = vec![vec![' '; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            ans[i][j] = sin[j][n - i - 1];
+        }
+    }
+    ans
+}
+
+// `poly_begin` で示された座標を左上とする
+fn add_poly(
+    s_base: &Vec<Vec<char>>,
+    s_added: &Vec<Vec<char>>,
+    poly_begin: (isize, isize),
+) -> Option<Vec<Vec<char>>> {
+    let mut ret = s_base.clone();
+
+    for i in 0..4 {
+        let i_i = i as isize + poly_begin.0;
+        for j in 0..4 {
+            if s_added[i][j] == '.' {
+                continue;
+            }
+
+            let j_i = j as isize + poly_begin.1;
+            if !(0..4).contains(&i_i) || !(0..4).contains(&j_i) {
+                if s_added[i][j] == '#' {
+                    // ポリオミノが範囲外に出る
+                    return None;
+                } else {
+                    continue;
+                }
+            }
+
+            let i_u = i_i as usize;
+            let j_u = j_i as usize;
+            if s_base[i_u][j_u] == '#' {
+                // ポリオミノが被る
+                return None;
+            }
+
+            ret[i_u][j_u] = s_added[i][j];
+        }
+    }
+
+    Some(ret)
+}
 
 fn main() {
     input! {
+        p0: [Chars; 4],
+        p1: [Chars; 4],
+        p2: [Chars; 4],
     }
+
+    // 平行移動の候補は最大で 4x4=16 per Polyomino
+    // 回転は 4 通り
+    // 合わせて 1 Polyomino に対し 16x4 = 64 通りの置き方がある
+    // これが 3 つあると 64x3 = 192 通りにしかならず, 全探索ができそう
+    // 実装はつらめ
+
+    let p0_t0 = p0;
+    let p0_t1 = turn_left(&p0_t0, 4);
+    let p0_t2 = turn_left(&p0_t1, 4);
+    let p0_t3 = turn_left(&p0_t2, 4);
+    let vp0 = vec![p0_t0, p0_t1, p0_t2, p0_t3];
+
+    let p1_t0 = p1;
+    let p1_t1 = turn_left(&p1_t0, 4);
+    let p1_t2 = turn_left(&p1_t1, 4);
+    let p1_t3 = turn_left(&p1_t2, 4);
+    let vp1 = vec![p1_t0, p1_t1, p1_t2, p1_t3];
+
+    let p2_t0 = p2;
+    let p2_t1 = turn_left(&p2_t0, 4);
+    let p2_t2 = turn_left(&p2_t1, 4);
+    let p2_t3 = turn_left(&p2_t2, 4);
+    let vp2 = vec![p2_t0, p2_t1, p2_t2, p2_t3];
+
+    let mut v = vec![];
+    let mass_base = vec![vec!['.'; 4]; 4];
+    for pp0 in vp0 {
+        for i in -3..=3 {
+            for j in -3..=3 {
+                let Some(mass) = add_poly(&mass_base, &pp0, (i, j)) else { continue; };
+                v.push(mass);
+            }
+        }
+    }
+
+    let mut vv = vec![];
+    while let Some(mass) = v.pop() {
+        for pp1 in &vp1 {
+            for i in -3..=3 {
+                for j in -3..=3 {
+                    let Some(mass1) = add_poly(&mass, pp1, (i, j)) else { continue; };
+                    vv.push(mass1);
+                }
+            }
+        }
+    }
+
+    while let Some(mass) = vv.pop() {
+        for pp2 in &vp2 {
+            for i in -3..=3 {
+                for j in -3..=3 {
+                    let Some(mass2) = add_poly(&mass, pp2, (i, j)) else { continue; };
+                    let mut sharp_num = 0;
+                    for ii in 0..4 {
+                        for jj in 0..4 {
+                            if mass2[ii][jj] == '#' {
+                                sharp_num += 1;
+                            }
+                        }
+                    }
+                    if sharp_num == 16 {
+                        println!("Yes");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    // for item in &v {
+    //     for i in 0..4 {
+    //         println!("{:?}", item[i]);
+    //     }
+    //     println!();
+    // }
+    // return;
+
+    println!("No");
 }
--- v1.70.0/abc/abc325/src/bin/e.rs
+++ v1.70.0/abc/abc325/src/bin/e.rs
@@ -1,19 +1,47 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::fastout;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::BTreeSet;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
+use std::collections::BTreeSet;
 
-// static DUMMY: usize = std::usize::MAX / 4;
+static DUMMY: u64 = u64::MAX / 4;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: u64,
+        b: u64,
+        c: u64,
+        dnn: [[u64; n]; n],
+    }
+
+    // editorial のように電車で移動 -> 社用車に移動, と図面を分けた方が賢いかも
+
+    let mut by_car = vec![DUMMY; n];
+    let mut by_train = vec![DUMMY; n];
+    // (総移動時間, now, car?)
+    let mut bts = BTreeSet::new();
+    bts.insert((0, 0, true));
+    while let Some(cur) = bts.pop_first() {
+        if cur.2 == true && by_car[cur.1] == DUMMY {
+            // 車で始めて到着した
+            // 車で来たので車で動ける
+            by_car[cur.1] = cur.0;
+            for i in 0..n {
+                if by_car[i] == DUMMY {
+                    bts.insert((cur.0 + dnn[cur.1][i] * a, i, true));
+                }
+            }
+        }
+        // 電車に乗り換えるのは前の手段に依らない
+        if by_train[cur.1] == DUMMY {
+            by_train[cur.1] = cur.0;
+            for i in 0..n {
+                if by_train[i] == DUMMY {
+                    bts.insert((cur.0 + dnn[cur.1][i] * b + c, i, false));
+                }
+            }
+        }
+    }
+
+    println!("{}", by_car[n - 1].min(by_train[n - 1]));
 }
