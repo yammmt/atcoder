fcdb42caeb29464e661d10da55246fa8bd66740b
yammmt <cu.cs.y20@gmail.com>
2023-11-14 21:16:40 +0900

    :tipping_hand_woman:

--- v1.70.0/abc/abc242/src/bin/d.rs
+++ v1.70.0/abc/abc242/src/bin/d.rs
@@ -1,16 +1,44 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
+use proconio::marker::Chars;
 
-// static DUMMY: usize = std::usize::MAX / 4;
+fn st_k(s: &Vec<char>, t: usize, k: usize) -> char {
+    let vc = ['A', 'B', 'C'];
+    if k == 0 {
+        let i = match s[0] {
+            'A' => 0,
+            'B' => 1,
+            'C' => 2,
+            _ => unreachable!(),
+        };
+        vc[(i + (t % 3)) % 3]
+    } else if t == 0 {
+        s[k]
+    } else {
+        let prev = st_k(s, t - 1, k / 2);
+        let i = match prev {
+            'A' => 0,
+            'B' => 1,
+            'C' => 2,
+            _ => unreachable!(),
+        };
+        if k % 2 == 0 {
+            vc[(i + 1) % 3]
+        } else {
+            vc[(i + 2) % 3]
+        }
+    }
+}
 
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+        q: usize,
+        tkq: [(usize, usize); q],
+    }
+
+    for tk in tkq {
+        let t = tk.0;
+        let k = tk.1 - 1;
+        println!("{}", st_k(&s, t, k));
+    }
 }
