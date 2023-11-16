f1bc04bc02ccee34e05652fe390aaa4476e00c03
yammmt <cu.cs.y20@gmail.com>
2023-11-16 21:36:00 +0900

    :vampire:

--- v1.70.0/abc/abc318/src/bin/e.rs
+++ v1.70.0/abc/abc318/src/bin/e.rs
@@ -1,16 +1,30 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
 
-// static DUMMY: usize = std::usize::MAX / 4;
+const A_MAX: usize = 300000;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        an: [usize; n],
+    }
+
+    // 左の候補数を覚える
+    // [1, 2, 3, 1, 4, 1] として, ans の増え方は
+    // [0, 1, 1, 0, 2, 0]
+    // すべての数字に対してこれを随時数えると TLE だが, 更新が必要なのは a[i] だけで高速化できそう
+    let mut ans = 0;
+    let mut left_cnt = vec![0; A_MAX + 1];
+    let mut confirmed_num = vec![0; A_MAX + 1];
+    let mut last_appeared = vec![-1isize; A_MAX + 1];
+    for (i, &a) in an.iter().enumerate() {
+        if last_appeared[a] > -1 {
+            confirmed_num[a] += left_cnt[a] * (i - last_appeared[a] as usize - 1);
+            ans += confirmed_num[a];
+        }
+
+        left_cnt[a] += 1;
+        last_appeared[a] = i as isize;
+    }
+
+    println!("{ans}");
 }
