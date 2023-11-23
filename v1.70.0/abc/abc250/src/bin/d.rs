69a50a070e82e02c77b07ba4af49f3b4ff4642c2
yammmt <cu.cs.y20@gmail.com>
2023-11-23 14:02:19 +0900

    :confounded:

--- v1.70.0/abc/abc250/src/bin/d.rs
+++ v1.70.0/abc/abc250/src/bin/d.rs
@@ -1,19 +1,61 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
+// 同方針: https://atcoder.jp/contests/abc250/editorial/3950
+
 use proconio::fastout;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::BTreeSet;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-
-// static DUMMY: usize = usize::MAX / 4;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+    }
+
+    // q も素数と読んでよい？
+    // N の三乗根以下の数を集めて, それぞれを q と仮置きする
+    // p の値が一意に定まれば pass
+
+    // N の三乗根の数
+    let mut q_max: usize = 1;
+    while q_max.pow(3) <= n {
+        q_max += 1;
+    }
+
+    // N の三乗根以下の素数判定
+    let mut is_prime = vec![true; q_max + 1];
+    is_prime[0] = false;
+    is_prime[1] = false;
+    for i in 2..=q_max {
+        if !is_prime[i] {
+            continue;
+        }
+
+        let mut j = i + i;
+        while j <= q_max {
+            is_prime[j] = false;
+            j += i;
+        }
+    }
+
+    let mut prime_num = vec![0; q_max + 1];
+    for i in 2..=q_max {
+        prime_num[i] = if is_prime[i] {
+            prime_num[i - 1] + 1
+        } else {
+            prime_num[i - 1]
+        };
+    }
+
+    // ひとつひとつ数え上げると 10^18 / 2^3 で TLE する
+    // 割った数以下の素数の数を予め線形時間で求めておく
+    let mut ans = 0;
+    for q in 2..=q_max {
+        if !is_prime[q] {
+            continue;
+        }
+
+        let qqq = q.pow(3);
+        let p_max = n / qqq;
+        ans += prime_num[p_max.min(q - 1)];
+    }
+
+    println!("{ans}");
 }
--- v1.70.0/abc/abc317/src/bin/e.rs
+++ v1.70.0/abc/abc317/src/bin/e.rs
@@ -1,16 +1,102 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
+use proconio::fastout;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
+use proconio::marker::Chars;
+use std::collections::VecDeque;
 
-// static DUMMY: usize = std::usize::MAX / 4;
+static DUMMY: usize = usize::MAX / 4;
 
+#[fastout]
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize,
+        ahw: [Chars; h],
+    }
+    let dir = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+    // 通れないマスを確定させる
+    // 行/列単位に左右/上下で判定する
+    let mut is_safe = vec![vec![true; w]; h];
+    for i in 0..h {
+        for j in 0..w {
+            if ahw[i][j] == 'S' || ahw[i][j] == 'G' || ahw[i][j] == '.' {
+                continue;
+            }
+
+            is_safe[i][j] = false;
+            let cur_d = match ahw[i][j] {
+                'v' => (1, 0),
+                '^' => (-1, 0),
+                '>' => (0, 1),
+                '<' => (0, -1),
+                _ => (0, 0),
+            };
+            if cur_d == (0, 0) {
+                continue;
+            }
+
+            let mut x = i as isize + cur_d.0;
+            let mut y = j as isize + cur_d.1;
+            loop {
+                if x < 0
+                    || y < 0
+                    || x >= h as isize
+                    || y >= w as isize
+                    || ahw[x as usize][y as usize] != '.'
+                {
+                    break;
+                }
+
+                is_safe[x as usize][y as usize] = false;
+                x += cur_d.0;
+                y += cur_d.1;
+            }
+        }
+    }
+
+    let mut start = (0, 0);
+    let mut goal = (0, 0);
+    for i in 0..h {
+        for j in 0..w {
+            if ahw[i][j] == 'S' {
+                start = (i, j);
+            } else if ahw[i][j] == 'G' {
+                goal = (i, j);
+            }
+        }
+    }
+
+    let mut que = VecDeque::new();
+    let mut costs = vec![vec![DUMMY; w]; h];
+    // (cost, (i, j))
+    que.push_back((0, start));
+    while let Some(cur) = que.pop_front() {
+        let c = cur.0;
+        let x = cur.1 .0;
+        let y = cur.1 .1;
+        if costs[x][y] != DUMMY {
+            continue;
+        }
+
+        costs[x][y] = c;
+        for d in &dir {
+            // println!("({x}, {y}) += ({}, {})", d.0, d.1);
+            let x_nxt = x.wrapping_add_signed(d.0);
+            let y_nxt = y.wrapping_add_signed(d.1);
+            if x_nxt >= h || y_nxt >= w || costs[x_nxt][y_nxt] != DUMMY || !is_safe[x_nxt][y_nxt] {
+                continue;
+            }
+
+            que.push_back((c + 1, (x_nxt, y_nxt)));
+        }
+    }
+
+    println!(
+        "{}",
+        if costs[goal.0][goal.1] == DUMMY {
+            -1
+        } else {
+            costs[goal.0][goal.1] as isize
+        }
+    );
 }
