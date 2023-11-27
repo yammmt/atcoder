d83bad57f84a328b19281c2b92fa0be1418b7942
yammmt <cu.cs.y20@gmail.com>
2023-11-27 21:28:54 +0900

    :ear_with_hearing_aid:

--- v1.70.0/abc/abc328/src/bin/c.rs
+++ v1.70.0/abc/abc328/src/bin/c.rs
@@ -1,20 +1,25 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::fastout;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::cmp::Reverse;
-// use std::collections::BinaryHeap;
-// use std::collections::BTreeSet;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-
-// static DUMMY: usize = usize::MAX / 4;
+use proconio::marker::Chars;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        q: usize,
+        s: Chars,
+        lrq: [(usize, usize); q],
+    }
+
+    let mut cusum = vec![0; n + 1];
+    for i in 2..=n {
+        cusum[i] = cusum[i - 1];
+        if s[i - 1] == s[i - 2] {
+            cusum[i] += 1;
+        }
+    }
+
+    for lr in lrq {
+        println!("{}", cusum[lr.1] - cusum[lr.0]);
+    }
 }
