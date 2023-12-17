930dd17673478519267a5a46e655c29bdaecec59
yammmt <cu.cs.y20@gmail.com>
2023-12-17 19:37:04 +0900

    :ok_man:

--- v1.70.0/abc/abc250/src/bin/e.rs
+++ v1.70.0/abc/abc250/src/bin/e.rs
@@ -1,19 +1,70 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::fastout;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::BTreeSet;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-
-// static DUMMY: usize = usize::MAX / 4;
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        an: [usize; n],
+        bn: [usize; n],
+        q: usize,
+        xyq: [(usize, usize); q],
+    }
+
+    let set_len = |an: &Vec<_>| {
+        let mut ret = vec![];
+        let mut cur = HashSet::new();
+        for a in an {
+            cur.insert(*a);
+            ret.push(cur.len());
+        }
+        ret
+    };
+    let a_set_len = set_len(&an);
+    let b_set_len = set_len(&bn);
+
+    let insert_order = |an: &Vec<_>| {
+        let mut ret = vec![];
+        let mut cur = HashSet::new();
+        for a in an {
+            if !cur.contains(a) {
+                cur.insert(a);
+                ret.push(*a);
+            }
+        }
+        ret
+    };
+    let a_inserted_order = insert_order(&an);
+    let b_inserted_order = insert_order(&bn);
+
+    let mut a_b_same = vec![];
+    let mut cur = HashSet::new();
+    for i in 0..a_inserted_order.len().min(b_inserted_order.len()) {
+        let a = a_inserted_order[i];
+        let b = b_inserted_order[i];
+        if cur.contains(&a) {
+            cur.remove(&a);
+        } else {
+            cur.insert(a);
+        }
+        if cur.contains(&b) {
+            cur.remove(&b);
+        } else {
+            cur.insert(b);
+        }
+
+        a_b_same.push(cur.is_empty());
+    }
+
+    for (x, y) in xyq {
+        println!(
+            "{}",
+            if a_set_len[x - 1] == b_set_len[y - 1] && a_b_same[a_set_len[x - 1] - 1] {
+                "Yes"
+            } else {
+                "No"
+            }
+        );
+    }
 }
---
+++ v1.70.0/abc/abc253/src/bin/d.rs
@@ -0,0 +1,41 @@
+use proconio::fastout;
+use proconio::input;
+
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    a / gcd(a, b) * b
+}
+
+#[fastout]
+fn main() {
+    input! {
+        n: u64,
+        a: u64,
+        b: u64,
+    }
+
+    // 全部足して an と bm を引いて ab 公倍数を足す
+
+    let ap_sum = |last_term| {
+        last_term * (1 + last_term) / 2
+    };
+
+    let mut n_sum = n * (1 + n) / 2;
+    let a_sum = a * ap_sum(n / a);
+    let b_sum = b * ap_sum(n / b);
+    let ab = lcm(a, b);
+    let ab_sum = ab * ap_sum(n / ab);
+
+    n_sum -= a_sum;
+    n_sum -= b_sum;
+    n_sum += ab_sum;
+
+    println!("{n_sum}");
+}
