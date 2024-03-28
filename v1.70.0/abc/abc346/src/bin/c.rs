9d5052348aab7ee0e9c616c62344684a92e563ba
yammmt <cu.cs.y20@gmail.com>
2024-03-28 20:39:11 +0900

    :leg:

--- v1.70.0/abc/abc346/src/bin/c.rs
+++ v1.70.0/abc/abc346/src/bin/c.rs
@@ -1,29 +1,25 @@
-// use ac_library::modint::ModInt1000000007 as Mint;
-// use ac_library::modint::ModInt998244353 as Mint;
-// use ac_library::SccGraph;
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::fastout;
 use proconio::input;
-// use proconio::marker::Bytes;
-// use proconio::marker::Chars;
-// use proconio::marker::Usize1;
-// use rand::rngs::SmallRng;
-// use rand::{Rng, SeedableRng};
-// use std::cmp::Ordering;
-// use std::cmp::Reverse;
-// use std::collections::BinaryHeap;
-// use std::collections::BTreeSet;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-
-// const DUMMY: usize = usize::MAX / 4;
-// const MOD: usize = 998_244_353;
-// const MOD: usize = 1_000_000_007;
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        an: [usize; n],
+    }
+
+    let mut ans = (1 + k) * k / 2;
+    let mut appeared = HashSet::new();
+    for a in an {
+        if a <= k {
+            appeared.insert(a);
+        }
+    }
+    for a in appeared {
+        ans -= a;
+    }
+
+    println!("{ans}");
 }
--- v1.70.0/abc/abc346/src/bin/d.rs
+++ v1.70.0/abc/abc346/src/bin/d.rs
@@ -1,29 +1,64 @@
-// use ac_library::modint::ModInt1000000007 as Mint;
-// use ac_library::modint::ModInt998244353 as Mint;
-// use ac_library::SccGraph;
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
+// 遷移ごっちゃになる, なんとかして
+// WA:
+// 2
+// 01
+// 100 200
+
 use proconio::fastout;
 use proconio::input;
-// use proconio::marker::Bytes;
-// use proconio::marker::Chars;
-// use proconio::marker::Usize1;
-// use rand::rngs::SmallRng;
-// use rand::{Rng, SeedableRng};
-// use std::cmp::Ordering;
-// use std::cmp::Reverse;
-// use std::collections::BinaryHeap;
-// use std::collections::BTreeSet;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
+use proconio::marker::Chars;
 
-// const DUMMY: usize = usize::MAX / 4;
-// const MOD: usize = 998_244_353;
-// const MOD: usize = 1_000_000_007;
+const DUMMY: usize = usize::MAX / 3;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        s: Chars,
+        cn: [usize; n],
+    }
+
+    // dp[i][j][k]: i 文字目まで見終わった際のコスト
+    // i は 0-origin
+    // j = 1 なら i-1 文字目は変わっている
+    // k = 1 なら i 文字目と i+1 文字目が一致する組は既出
+    let mut dp = vec![vec![vec![DUMMY; 2]; 2]; n + 1];
+    dp[0][0][0] = 0;
+    for i in 0..n {
+        // i 文字目を変えない
+        if i == 0 {
+            dp[1][0][0] = 0;
+        } else if s[i] == s[i - 1] {
+            // 直前を変えていなければ, 今回は一致
+            dp[i + 1][0][1] = dp[i + 1][0][1].min(dp[i][0][0]);
+            // 直前を変えていれば, 今回は不一致
+            dp[i + 1][0][0] = dp[i + 1][0][0].min(dp[i][1][0]);
+            dp[i + 1][0][1] = dp[i + 1][0][1].min(dp[i][1][1]);
+        } else {
+            // 直前を変えていなければ, 今回は不一致
+            dp[i + 1][0][0] = dp[i + 1][0][0].min(dp[i][0][0]);
+            dp[i + 1][0][1] = dp[i + 1][0][1].min(dp[i][0][1]);
+            // 直前を変えていれば, 今回は一致
+            dp[i + 1][0][1] = dp[i + 1][0][1].min(dp[i][1][0]);
+        }
+
+        // i 文字目を変える
+        if i == 0 {
+            dp[1][1][0] = cn[0];
+        } else if s[i] == s[i - 1] {
+            // 直前を変えていなければ, 今回は不一致
+            dp[i + 1][1][0] = dp[i + 1][1][0].min(dp[i][0][0] + cn[i]);
+            dp[i + 1][1][1] = dp[i + 1][1][1].min(dp[i][0][1] + cn[i]);
+            // 直前を変えていれば, 今回は一致
+            dp[i + 1][1][1] = dp[i + 1][1][1].min(dp[i][1][0] + cn[i]);
+        } else {
+            // 直前を変えていなければ, 今回は一致
+            dp[i + 1][1][1] = dp[i + 1][1][1].min(dp[i][0][0] + cn[i]);
+            // 直前を変えていれば, 今回は不一致
+            dp[i + 1][1][0] = dp[i + 1][1][0].min(dp[i][1][0] + cn[i]);
+            dp[i + 1][1][1] = dp[i + 1][1][1].min(dp[i][1][1] + cn[i]);
+        }
+    }
+
+    println!("{}", dp[n][0][1].min(dp[n][1][1]));
 }
