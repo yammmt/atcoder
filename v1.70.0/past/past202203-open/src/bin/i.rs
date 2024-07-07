49973f1fa1658e8dbf52271c69a96c75020140b2
yammmt <cu.cs.y20@gmail.com>
2024-07-07 16:31:05 +0900

    :bow:

--- v1.70.0/past/past202203-open/src/bin/i.rs
+++ v1.70.0/past/past202203-open/src/bin/i.rs
@@ -1,29 +1,63 @@
-// use ac_library::modint::ModInt1000000007 as Mint;
-// use ac_library::modint::ModInt998244353 as Mint;
-// use ac_library::SccGraph;
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::fastout;
 use proconio::input;
-// use proconio::marker::Bytes;
-// use proconio::marker::Chars;
-// use proconio::marker::Usize1;
-// use rand::rngs::SmallRng;
-// use rand::{Rng, SeedableRng};
-// use std::cmp::Ordering;
-// use std::cmp::Reverse;
-// use std::collections::BinaryHeap;
-// use std::collections::BTreeSet;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-
-// const DUMMY: usize = usize::MAX / 4;
-// const MOD: usize = 998_244_353;
-// const MOD: usize = 1_000_000_007;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        mut sn: [(isize, isize); n],
+        mut tn: [(isize, isize); n],
+    }
+
+    // S の任意の一点を T の任意の一点に動かし, 残りの点も同じように動かす, では O(N^2)
+    // 操作をしても x/y 座標の片方はまったく変わらない
+    // 対称の基準となる軸は (最大-最小)/2 じゃないかと思うがうまい説明ができない
+    // なんにせよ操作不要と x/y 片方基準の平行移動を二度とで三度全一致を試せばよいだけ？
+    // 0.5 が出てくる場合があるが, 0.5 なら誤差は出ないので無視してよいはず
+    // あるいは入力全部倍にすれば解決する？
+    // そもそも平行移動の式に x2 が出るから無視できるのか
+
+    // 元から一致
+    sn.sort_unstable();
+    tn.sort_unstable();
+    if sn == tn {
+        println!("Yes");
+        return;
+    }
+
+    let sy_min = sn.iter().map(|p| p.1).min().unwrap();
+    let sy_max = sn.iter().map(|p| p.1).max().unwrap();
+    let ty_min = tn.iter().map(|p| p.1).min().unwrap();
+    let ty_max = tn.iter().map(|p| p.1).max().unwrap();
+
+    let sx_min = sn.iter().map(|p| p.0).min().unwrap();
+    let sx_max = sn.iter().map(|p| p.0).max().unwrap();
+    let tx_min = tn.iter().map(|p| p.0).min().unwrap();
+    let tx_max = tn.iter().map(|p| p.0).max().unwrap();
+
+    // x 軸に平行な線に対称となる移動
+    let y_mid_x2 = sy_max.max(ty_max) + sy_min.min(ty_min);
+    let mut s_x = Vec::with_capacity(n);
+    for s in &sn {
+        s_x.push((s.0, y_mid_x2 - s.1));
+    }
+    s_x.sort_unstable();
+    if s_x == tn {
+        println!("Yes");
+        return;
+    }
+
+    // y 軸に平行な線に対称となる移動
+    let x_mid_x2 = sx_max.max(tx_max) + sx_min.min(tx_min);
+    let mut s_y = Vec::with_capacity(n);
+    for s in &sn {
+        s_y.push((x_mid_x2 - s.0, s.1));
+    }
+    s_y.sort_unstable();
+    if s_y == tn {
+        println!("Yes");
+        return;
+    }
+
+    println!("No");
 }
--- v1.70.0/past/past202203-open/src/bin/j.rs
+++ v1.70.0/past/past202203-open/src/bin/j.rs
@@ -1,29 +1,83 @@
-// use ac_library::modint::ModInt1000000007 as Mint;
-// use ac_library::modint::ModInt998244353 as Mint;
-// use ac_library::SccGraph;
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
+use ac_library::modint::ModInt998244353 as Mint;
 use proconio::fastout;
 use proconio::input;
-// use proconio::marker::Bytes;
-// use proconio::marker::Chars;
-// use proconio::marker::Usize1;
-// use rand::rngs::SmallRng;
-// use rand::{Rng, SeedableRng};
-// use std::cmp::Ordering;
-// use std::cmp::Reverse;
-// use std::collections::BinaryHeap;
-// use std::collections::BTreeSet;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
 
-// const DUMMY: usize = usize::MAX / 4;
-// const MOD: usize = 998_244_353;
-// const MOD: usize = 1_000_000_007;
+const MOD: usize = 998_244_353;
+
+struct Com {
+    fac: Vec<usize>,
+    finv: Vec<usize>,
+    inv: Vec<usize>,
+    mod_base: usize,
+}
+
+impl Com {
+    fn new(n: usize, m: usize) -> Self {
+        let mut com = Com {
+            fac: vec![0; n + 1],
+            inv: vec![0; n + 1],
+            finv: vec![0; n + 1],
+            mod_base: m,
+        };
+
+        com.fac[0] = 1;
+        com.fac[1] = 1;
+        com.inv[1] = 1;
+        com.finv[0] = 1;
+        com.finv[1] = 1;
+
+        for i in 2..n + 1 {
+            com.fac[i] = com.fac[i - 1] * i % com.mod_base;
+            com.inv[i] =
+                com.mod_base - com.inv[com.mod_base % i] * (com.mod_base / i) % com.mod_base;
+            com.finv[i] = com.finv[i - 1] * com.inv[i] % com.mod_base;
+        }
+
+        com
+    }
+
+    // nCk mod m
+    fn com(&self, n: usize, k: usize) -> usize {
+        if n < k {
+            return 0;
+        }
+
+        self.fac[n] * (self.finv[k] * self.finv[n - k] % self.mod_base) % self.mod_base
+    }
+}
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        mut an: [usize; n],
+    }
+    an.sort_unstable();
+
+    // ai は相異なるので考察がちょっと楽できそう
+    // 割り算しなくとも最大値全部足して最小値全部引けば求まりそう
+    // 全体の最大値が区間最大値となる場合は自身が選ばれる + 自身より大きい要素が選ばれない
+    // 二項係数を前計算して使い回すやつっぽい
+
+    // => sounansya さん解説と同じっぽい
+
+    let com = Com::new(n, MOD);
+
+    let mut ans = Mint::new(0);
+    for i in 0..n - k + 1 {
+        // an[n-i-1] が最大値となる場合の数は, 自身が選ばれかつ残りがすべて自身より小さい場合の数
+        // 自身より小さい数が n-i-1 個
+        ans += an[n - i - 1] * com.com(n - i - 1, k - 1);
+    }
+
+    for i in 0..n - k + 1 {
+        // 自身より大きい数は n-i-1 個
+        ans -= an[i] * com.com(n - i - 1, k - 1);
+    }
+
+    // 期待値だから割る
+    ans /= com.com(n, k);
+
+    println!("{ans}");
 }
