4f09e6c495081be9383dbd4a205a59b83ca0821b
yammmt <cu.cs.y20@gmail.com>
2025-05-15 21:50:42 +0900

    :detective:

--- v1.70.0/abc/abc404/src/bin/d.rs
+++ v1.70.0/abc/abc404/src/bin/d.rs
@@ -1,31 +1,61 @@
-// use ac_library::modint::ModInt1000000007 as Mint;
-// use ac_library::modint::ModInt998244353 as Mint;
-// use ac_library::SccGraph;
-// use itertools::Itertools;
-// use ordered_float::NotNan;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::fastout;
 use proconio::input;
-// use proconio::marker::Bytes;
-// use proconio::marker::Chars;
-// use proconio::marker::Usize1;
-// use rand::rngs::SmallRng;
-// use rand::{Rng, SeedableRng};
-// use std::cmp::Ordering;
-// use std::cmp::Reverse;
-// use std::collections::BinaryHeap;
-// use std::collections::BTreeSet;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use superslice::Ext;
-
-// const DUMMY: usize = usize::MAX / 4;
-// const MOD: usize = 998_244_353;
-// const MOD: usize = 1_000_000_007;
+use proconio::marker::Usize1;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize,
+        cn: [usize; n],
+    }
+    let mut km = Vec::with_capacity(m);
+    let mut amk = Vec::with_capacity(m);
+    for _ in 0..m {
+        input! {
+            k: usize,
+            ak: [Usize1; k],
+        }
+        km.push(k);
+        amk.push(ak);
+    }
+
+    let mut park_to_animals = vec![vec![]; n];
+    for (i, ak) in amk.iter().enumerate() {
+        for &a in ak {
+            park_to_animals[a].push(i);
+        }
+    }
+
+    let mut ans = usize::MAX / 3;
+    for bit in 0..3usize.pow(n as u32) {
+        let mut visited_count = vec![0; m];
+        let mut b = bit;
+        let mut cost_cur = 0;
+        for i in 0..n {
+            match b % 3 {
+                0 => {}
+                1 => {
+                    for &a in &park_to_animals[i] {
+                        visited_count[a] += 1;
+                    }
+                    cost_cur += cn[i];
+                }
+                2 => {
+                    for &a in &park_to_animals[i] {
+                        visited_count[a] += 2;
+                    }
+                    cost_cur += 2 * cn[i];
+                }
+                _ => unreachable!(),
+            }
+            b /= 3;
+        }
+
+        if visited_count.iter().all(|&v| v >= 2) {
+            ans = ans.min(cost_cur);
+        }
+    }
+
+    println!("{ans}");
 }
