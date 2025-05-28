2b0c7af2ad4f31bef18fe4ffb587e39be529a4a9
yammmt <cu.cs.y20@gmail.com>
2025-05-28 21:38:34 +0900

    :question: :question: :question:

--- v1.70.0/abc/abc190/src/bin/f.rs
+++ v1.70.0/abc/abc190/src/bin/f.rs
@@ -1,16 +1,115 @@
-// use itertools::Itertools;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::input;
-// use proconio::marker::Chars;
-// use std::cmp::Ordering;
-// use std::collections::BinaryHeap;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
 
-// static DUMMY: usize = std::usize::MAX / 4;
+static INF: usize = std::usize::MAX / 2;
+
+#[derive(Debug)]
+struct SegTree<T, F>
+where
+    T: Clone,
+    F: Fn(&T, &T) -> T,
+{
+    n: usize,
+    size: usize,
+    node: Vec<T>,
+    /// 単位元
+    identity_e: T,
+    /// 2 つのデータから値を合成するための関数
+    combine_f: F,
+}
+
+impl<T, F> SegTree<T, F>
+where
+    T: Clone,
+    F: Fn(&T, &T) -> T,
+{
+    fn new(n: usize, identity_e: T, combine_f: F) -> Self {
+        let mut size = 1;
+        while size < n {
+            size *= 2;
+        }
+        let node = vec![identity_e.clone(); 2 * size];
+
+        SegTree {
+            n,
+            size,
+            node,
+            identity_e,
+            combine_f,
+        }
+    }
+
+    /// 配列の各要素を登録する
+    #[allow(dead_code)]
+    fn build(&mut self, array: &[T]) {
+        assert_eq!(array.len(), self.n);
+        for (i, &ref a) in array.iter().enumerate() {
+            self.node[i + self.size] = a.clone();
+        }
+        for i in (1..self.size).rev() {
+            // {左,右} の子
+            self.node[i] = (self.combine_f)(&self.node[i << 1 | 0], &self.node[i << 1 | 1]);
+        }
+    }
+
+    /// [一点更新] 位置を値で更新
+    #[allow(dead_code)]
+    fn update(&mut self, index: usize, value: T) {
+        let mut i = self.size + index;
+        self.node[i] = value;
+        while i > 1 {
+            i >>= 1;
+            self.node[i] = (self.combine_f)(&self.node[i << 1 | 0], &self.node[i << 1 | 1]);
+        }
+    }
+
+    /// [区間取得] 区間 [l, r) 内の `combine_f` を順々に適用した結果
+    fn fold(&self, mut l: usize, mut r: usize) -> T {
+        l += self.size;
+        r += self.size;
+        let mut value_l = self.identity_e.clone();
+        let mut value_r = self.identity_e.clone();
+        while l < r {
+            if l & 1 > 0 {
+                value_l = (self.combine_f)(&value_l, &self.node[l]);
+                l += 1;
+            }
+            if r & 1 > 0 {
+                r -= 1;
+                value_r = (self.combine_f)(&self.node[r], &value_r);
+            }
+            l >>= 1;
+            r >>= 1;
+        }
+        (self.combine_f)(&value_l, &value_r)
+    }
+}
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        an: [usize; n],
+    }
+
+    // a_i より値が小さい場所で 1 を, それ以外で 0 をとる数列
+
+    let mut a_to_pos = vec![INF; n];
+    for (i, &a) in an.iter().enumerate() {
+        a_to_pos[a] = i;
+    }
+
+    let mut st = SegTree::new(n, 0, |a, b| a + b);
+    let mut inversion = 0;
+    for i in 0..n {
+        let pos = a_to_pos[i];
+        inversion += st.fold(pos, n);
+        st.update(pos, 1);
+    }
+
+    for i in 0..n {
+        println!("{}", inversion);
+        // an[i] より大
+        inversion += n - an[i] - 1;
+        // an[i] より小
+        inversion -= an[i];
+    }
 }
