2f4d114b64182b3699f68461e1fbf56e1f302079
yammmt <cu.cs.y20@gmail.com>
2025-06-09 21:35:41 +0900

    :venezuela:

--- csharp/7.0.7/past/past201912-open/h.cs
+++ csharp/7.0.7/past/past201912-open/h.cs
@@ -6,5 +6,110 @@ class Program
 {
     static void Main(string[] args)
     {
+        int n = int.Parse(Console.ReadLine());
+        long[] cn = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+        int q = int.Parse(Console.ReadLine());
+
+        // origin 違うので問題文と偶数奇数が逆
+
+        // (idx, 在庫数)
+        (int, long) evenMin = (0, 1_000_000_002);
+        (int, long) oddMin = (0, 1_000_000_002);
+        long[] soldOne = new long[n];
+        long soldEven = 0;
+        long soldAll = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                if (cn[i] < evenMin.Item2)
+                {
+                    evenMin.Item1 = i;
+                    evenMin.Item2 = cn[i];
+                }
+            }
+            else
+            {
+                if (cn[i] < oddMin.Item2)
+                {
+                    oddMin.Item1 = i;
+                    oddMin.Item2 = cn[i];
+                }
+            }
+        }
+
+        long ans = 0;
+
+        for (int i = 0; i < q; i++)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            switch (int.Parse(s[0]))
+            {
+                case 1:
+                    {
+                        // x を a 枚販売する
+                        int x = int.Parse(s[1]) - 1;
+                        long a = long.Parse(s[2]);
+                        long sold = soldOne[x] + soldAll;
+                        if (x % 2 == 0)
+                        {
+                            sold += soldEven;
+                        }
+                        if (sold + a <= cn[x])
+                        {
+                            ans += a;
+                            soldOne[x] += a;
+
+                            if (x % 2 == 0 && cn[x] - soldOne[x] < evenMin.Item2)
+                            {
+                                evenMin = (x, cn[x] - soldOne[x]);
+                            }
+                            else if (x % 2 == 1 && cn[x] - soldOne[x] < oddMin.Item2)
+                            {
+                                oddMin = (x, cn[x] - soldOne[x]);
+                            }
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        // 偶数カードを a 枚ずつ販売する
+                        long a = long.Parse(s[1]);
+                        if (evenMin.Item2 - soldEven - soldAll - a < 0)
+                        {
+                            // 販売できない
+                            break;
+                        }
+
+                        soldEven += a;
+                        // 端数切り上げ
+                        ans += a * ((n + 1) / 2);
+                        break;
+                    }
+                case 3:
+                    {
+                        // 全種類 a 枚ずつ販売する
+                        long a = long.Parse(s[1]);
+                        if (
+                            evenMin.Item2 - soldEven - soldAll - a < 0
+                            || oddMin.Item2 - soldAll - a < 0
+                        )
+                        {
+                            break;
+                        }
+
+                        soldAll += a;
+                        ans += a * n;
+                        break;
+                    }
+                default:
+                    // unreachable
+                    break;
+            }
+            // Console.WriteLine("i: " + i + ", ans: " + ans);
+        }
+
+        Console.WriteLine(ans);
     }
 }
--- csharp/7.0.7/past/past201912-open/i.cs
+++ csharp/7.0.7/past/past201912-open/i.cs
@@ -6,5 +6,49 @@ class Program
 {
     static void Main(string[] args)
     {
+        string[] s = Console.ReadLine().Split(' ');
+        int n = int.Parse(s[0]);
+        int m = int.Parse(s[1]);
+
+        (int, long)[] scm = new (int, long)[m];
+        for (int i = 0; i < m; i++)
+        {
+            string[] ss = Console.ReadLine().Split(' ');
+            char[] c = ss[0].ToCharArray();
+            int bit = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (c[j] == 'Y')
+                {
+                    bit += (1 << j);
+                }
+            }
+
+            scm[i] = (bit, long.Parse(ss[1]));
+        }
+
+        int dpLen = 1 << n;
+        var dp = new long[dpLen];
+        Array.Fill(dp, 1_000_000_000_000L);
+        dp[0] = 0;
+
+        for (int i = 0; i < dpLen; i++)
+        {
+            // dummy 避けはあった方が高速な気もするが
+            for (int j = 0; j < m; j++)
+            {
+                int sNext = i | scm[j].Item1;
+                dp[sNext] = Math.Min(dp[sNext], dp[i] + scm[j].Item2);
+            }
+        }
+
+        if (dp[dpLen - 1] == 1_000_000_000_000L)
+        {
+            Console.WriteLine("-1");
+        }
+        else
+        {
+            Console.WriteLine(dp[dpLen - 1]);
+        }
     }
 }
