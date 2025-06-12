2622bb602687a36ab778008271ab12423e869a64
yammmt <cu.cs.y20@gmail.com>
2025-06-12 22:32:48 +0900

    :yemen:

--- csharp/7.0.7/past/past202004-open/c.cs
+++ csharp/7.0.7/past/past202004-open/c.cs
@@ -1,6 +1,52 @@
+using System;
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+        var grid = new char[n, 2 * n - 1];
+        for (int i = 0; i < n; i++)
+        {
+            string line = Console.ReadLine();
+            for (int j = 0; j < 2 * n - 1; j++)
+            {
+                grid[i, j] = line[j];
+            }
+        }
+        (int, int)[] dir = { (1, -1), (1, 0), (1, 1) };
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            var grid_new = new char[n, 2 * n - 1];
+            Array.Copy(grid, grid_new, grid.Length);
+            for (int j = 1; j < 2 * n - 2; j++)
+            {
+                if (grid[i, j] != '#')
+                {
+                    continue;
+                }
+
+                foreach ((int, int) d in dir)
+                {
+                    var ii = i + d.Item1;
+                    var jj = j + d.Item2;
+                    if (ii >= 0 && jj >= 0 && ii < n && jj < 2 * n - 1 && grid[ii, jj] == 'X')
+                    {
+                        grid_new[i, j] = 'X';
+                    }
+                }
+            }
+            grid = grid_new;
+        }
+
+        for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < 2 * n - 1; j++)
+                {
+                    Console.Write(grid[i, j]);
+                }
+                Console.WriteLine();
+            }
     }
 }
--- csharp/7.0.7/past/past202004-open/d.cs
+++ csharp/7.0.7/past/past202004-open/d.cs
@@ -1,6 +1,76 @@
+using System;
+
 class Program
 {
-    static void Main(string[] args)
+    static bool IsMatch(char a, char b)
     {
+        if (a == '.' || a == b)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    static void Main()
+    {
+        string abc = "abcdefghijklmnopqrstuvwxyz.";
+
+        // 問題文が難しい..
+        string s = Console.ReadLine();
+
+        int ans = 0;
+
+        // 一文字
+        for (int i = 0; i < abc.Length; i++)
+        {
+            for (int j = 0; j < s.Length; j++)
+            {
+                if (IsMatch(abc[i], s[j]))
+                {
+                    ans++;
+                    break;
+                }
+            }
+        }
+
+        // 二文字
+        for (int i = 0; i < abc.Length; i++)
+        {
+            for (int j = 0; j < abc.Length; j++)
+            {
+                for (int k = 0; k < s.Length - 1; k++)
+                {
+                    if (IsMatch(abc[i], s[k]) && IsMatch(abc[j], s[k + 1]))
+                    {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // 三文字
+        for (int i = 0; i < abc.Length; i++)
+        {
+            for (int j = 0; j < abc.Length; j++)
+            {
+                for (int k = 0; k < abc.Length; k++)
+                {
+                    for (int l = 0; l < s.Length - 2; l++)
+                    {
+                        if (IsMatch(abc[i], s[l]) && IsMatch(abc[j], s[l + 1]) && IsMatch(abc[k], s[l + 2]))
+                        {
+                            ans++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(ans);
     }
 }
