0b120c08f0b386ad79575c509e33fa3825f8f334
yammmt <cu.cs.y20@gmail.com>
2025-06-13 21:24:46 +0900

    :zombie_man:

--- csharp/7.0.7/past/past201912-open/k.cs
+++ csharp/7.0.7/past/past201912-open/k.cs
@@ -4,7 +4,65 @@ using System.Linq;
 
 class Program
 {
+    static (int pre, int post)[] EulerTour(int root, List<List<int>> edges)
+    {
+        int time = 0;
+        var ret = new (int pre, int post)[edges.Count];
+
+        void Dfs(int v_cur, int v_prev)
+        {
+            ret[v_cur].pre = time++;
+            foreach (int v_next in edges[v_cur])
+            {
+                if (v_next != v_prev)
+                {
+                    Dfs(v_next, v_cur);
+                }
+            }
+            ret[v_cur].post = time++;
+        }
+
+        Dfs(root, -1);
+
+        return ret;
+    }
+
     static void Main(string[] args)
     {
+        var n = int.Parse(Console.ReadLine());
+        var pn = Enumerable.Range(0, n)
+            .Select(_ => int.Parse(Console.ReadLine()) - 1)
+            .ToArray();
+        var q = int.Parse(Console.ReadLine());
+        var abq = Enumerable.Range(0, q)
+            .Select(_ =>
+            {
+                var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                return (a: ab[0] - 1, b: ab[1] - 1);
+            }).ToArray();
+
+        // 上司から部下を辿るため, 向きを逆にする
+        var edges = Enumerable.Range(0, n).Select(_ => new List<int>()).ToList();
+        int president = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (pn[i] == -2)
+            {
+                president = i;
+                continue;
+            }
+
+            edges[pn[i]].Add(i);
+        }
+
+        var ranks = EulerTour(president, edges);
+
+        foreach (var (a, b) in abq)
+        {
+            // a が b の部下 = b は a の上司
+            // 上司 in -> 部下 in -> 部下 out -> 上司 out
+            bool isSubordinate = ranks[b].pre < ranks[a].pre && ranks[a].post < ranks[b].post;
+            Console.WriteLine(isSubordinate ? "Yes" : "No");
+        }
     }
 }
