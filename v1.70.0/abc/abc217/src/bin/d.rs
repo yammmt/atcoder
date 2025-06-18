d0632adf22b5556a2f434f5d1746e2f067bffe02
yammmt <cu.cs.y20@gmail.com>
2025-06-18 21:00:30 +0900

    :egg:

--- v1.70.0/abc/abc217/src/bin/d.rs
+++ v1.70.0/abc/abc217/src/bin/d.rs
@@ -1,31 +1,116 @@
-// use ac_library::modint::ModInt1000000007 as Mint;
-// use ac_library::modint::ModInt998244353 as Mint;
-// use ac_library::SccGraph;
-// use itertools::Itertools;
-// use ordered_float::NotNan;
-// use permutohedron::heap_recursive;
-// use petgraph::unionfind::UnionFind;
 use proconio::fastout;
 use proconio::input;
-// use proconio::marker::Bytes;
-// use proconio::marker::Chars;
-// use proconio::marker::Usize1;
-// use rand::rngs::SmallRng;
-// use rand::{Rng, SeedableRng};
-// use std::cmp::Ordering;
-// use std::cmp::Reverse;
-// use std::collections::BinaryHeap;
-// use std::collections::BTreeSet;
-// use std::collections::HashSet;
-// use std::collections::HashMap;
-// use std::collections::VecDeque;
-// use superslice::Ext;
+use std::collections::HashMap;
 
-// const DUMMY: usize = usize::MAX / 4;
-// const MOD: usize = 998_244_353;
-// const MOD: usize = 1_000_000_007;
+#[derive(Debug)]
+struct SegTree<T, F>
+where
+    T: Clone,
+    F: Fn(&T, &T) -> T,
+{
+    n: usize,
+    size: usize,
+    node: Vec<T>,
+    /// 単位元
+    identity_e: T,
+    /// 2 つのデータから値を合成するための関数
+    combine_f: F,
+}
+
+impl<T, F> SegTree<T, F>
+where
+    T: Clone,
+    F: Fn(&T, &T) -> T,
+{
+    fn new(n: usize, identity_e: T, combine_f: F) -> Self {
+        let mut size = 1;
+        while size < n {
+            size *= 2;
+        }
+        let node = vec![identity_e.clone(); 2 * size];
+
+        SegTree {
+            n,
+            size,
+            node,
+            identity_e,
+            combine_f,
+        }
+    }
+
+    /// 配列の各要素を登録する
+    fn build(&mut self, array: &[T]) {
+        assert_eq!(array.len(), self.n);
+        for (i, &ref a) in array.iter().enumerate() {
+            self.node[i + self.size] = a.clone();
+        }
+        // 0 はダミーノードだから回さなくてよい
+        // ノード i の左を 2i, 右を 2i+1 で表すために好都合
+        for i in (1..self.size).rev() {
+            // {左,右} の子
+            self.node[i] = (self.combine_f)(&self.node[i << 1 | 0], &self.node[i << 1 | 1]);
+        }
+    }
+
+    /// [一点更新] 位置を値で更新
+    fn update(&mut self, index: usize, value: T) {
+        let mut i = self.size + index;
+        self.node[i] = value;
+        while i > 1 {
+            i >>= 1;
+            self.node[i] = (self.combine_f)(&self.node[i << 1 | 0], &self.node[i << 1 | 1]);
+        }
+    }
+
+    /// [区間取得] 区間 [l, r) 内の `combine_f` を順々に適用した結果
+    fn fold(&self, mut l: usize, mut r: usize) -> T {
+        l += self.size;
+        r += self.size;
+        let mut value_l = self.identity_e.clone();
+        let mut value_r = self.identity_e.clone();
+        while l < r {
+            if l & 1 > 0 {
+                value_l = (self.combine_f)(&value_l, &self.node[l]);
+                l += 1;
+            }
+            if r & 1 > 0 {
+                r -= 1;
+                value_r = (self.combine_f)(&self.node[r], &value_r);
+            }
+            l >>= 1;
+            r >>= 1;
+        }
+        (self.combine_f)(&value_l, &value_r)
+    }
+}
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        l: usize,
+        q: usize,
+        cxq: [(usize, usize); q],
+    }
+    let mut positions = cxq.iter().map(|&cx| cx.1).collect::<Vec<usize>>();
+    positions.sort_unstable();
+    positions.dedup();
+    let n = positions.len();
+    let mut pos_to_n = HashMap::new();
+    for (i, p) in positions.iter().enumerate() {
+        pos_to_n.insert(p, i);
+    }
+
+    let mut st_l = SegTree::new(n, 0, |a, b| *a.max(b));
+    let mut st_r = SegTree::new(n, l, |a, b| *a.min(b));
+    for (c, x) in cxq {
+        let i = *pos_to_n.get(&x).unwrap();
+        if c == 1 {
+            st_l.update(i, x);
+            st_r.update(i, x);
+        } else {
+            let l = st_l.fold(0, i);
+            let r = st_r.fold(i, n);
+            println!("{}", r - l);
+        }
+    }
 }
--- v1.70.0/past/past202109-open/src/bin/j.rs
+++ v1.70.0/past/past202109-open/src/bin/j.rs
@@ -1,27 +1,111 @@
-// セグ木か BIT, 後回し
-
+use proconio::fastout;
 use proconio::input;
-// use std::cmp::Order;
 
-fn main() {
-    input! {
-        // n: usize,
-        // q: usize,
-        // tkq: [(usize, usize); q],
+#[derive(Debug)]
+struct SegTree<T, F>
+where
+    T: Clone,
+    F: Fn(&T, &T) -> T,
+{
+    n: usize,
+    size: usize,
+    node: Vec<T>,
+    /// 単位元
+    identity_e: T,
+    /// 2 つのデータから値を合成するための関数
+    combine_f: F,
+}
+
+impl<T, F> SegTree<T, F>
+where
+    T: Clone,
+    F: Fn(&T, &T) -> T,
+{
+    fn new(n: usize, identity_e: T, combine_f: F) -> Self {
+        let mut size = 1;
+        while size < n {
+            size *= 2;
+        }
+        let node = vec![identity_e.clone(); 2 * size];
+
+        SegTree {
+            n,
+            size,
+            node,
+            identity_e,
+            combine_f,
+        }
     }
 
-    // 愚直に反転し続けるでは O(N^2)
-    // 反転条件をいもす法でも O(N^2)
+    /// 配列の各要素を登録する
+    fn build(&mut self, array: &[T]) {
+        assert_eq!(array.len(), self.n);
+        for (i, &ref a) in array.iter().enumerate() {
+            self.node[i + self.size] = a.clone();
+        }
+        // 0 はダミーノードだから回さなくてよい
+        // ノード i の左を 2i, 右を 2i+1 で表すために好都合
+        for i in (1..self.size).rev() {
+            // {左,右} の子
+            self.node[i] = (self.combine_f)(&self.node[i << 1 | 0], &self.node[i << 1 | 1]);
+        }
+    }
 
-    // for tk in tkq {
-    //     let t = tk.0;
-    //     let k = tk.1;
-    //     match k {
-    //         1 => {
-    //         }
-    //         2 => {
-    //         }
-    //         _ => unreachable!(),
-    //     }
-    // }
+    /// [一点更新] 位置を値で更新
+    fn update(&mut self, index: usize, value: T) {
+        let mut i = self.size + index;
+        self.node[i] = value;
+        while i > 1 {
+            i >>= 1;
+            self.node[i] = (self.combine_f)(&self.node[i << 1 | 0], &self.node[i << 1 | 1]);
+        }
+    }
+
+    /// [区間取得] 区間 [l, r) 内の `combine_f` を順々に適用した結果
+    fn fold(&self, mut l: usize, mut r: usize) -> T {
+        l += self.size;
+        r += self.size;
+        let mut value_l = self.identity_e.clone();
+        let mut value_r = self.identity_e.clone();
+        while l < r {
+            if l & 1 > 0 {
+                value_l = (self.combine_f)(&value_l, &self.node[l]);
+                l += 1;
+            }
+            if r & 1 > 0 {
+                r -= 1;
+                value_r = (self.combine_f)(&self.node[r], &value_r);
+            }
+            l >>= 1;
+            r >>= 1;
+        }
+        (self.combine_f)(&value_l, &value_r)
+    }
+}
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+        tkq: [(usize, usize); q],
+    }
+
+    // 左から j 番目/2n-j+1 番目の要素, 反転の組となるものをまとめて考える.
+    // 反転クエリでは最初の k 枚を反転させればよい. として一点加算区間和問題
+
+    let mut st = SegTree::new(n, 0, |&a, &b| a + b);
+    for (t, k) in tkq {
+        if t == 1 {
+            // セグメント木上で何番目に相当する要素かを求める
+            let pos = if k <= n { n + 1 - k } else { k - n };
+            let parity = (st.fold(pos - 1, n) + if k > n { 1 } else { 0 }) % 2;
+            println!("{}", if parity == 0 { n + 1 - pos } else { n + pos });
+        } else {
+            // ひっくり返す, 偶奇が変化
+            // ここの `fold` は k-1 一点の値を取得するもの
+            let sum_value = st.fold(k - 1, k);
+            st.update(k - 1, sum_value + 1);
+        }
+    }
 }
