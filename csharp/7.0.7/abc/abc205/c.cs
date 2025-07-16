73a03c67432c954b38eea1d131f38f1507d626ad
yammmt <cu.cs.y20@gmail.com>
2025-07-16 22:13:14 +0900

    :sweat:

--- csharp/7.0.7/abc/abc205/c.cs
+++ csharp/7.0.7/abc/abc205/c.cs
@@ -6,5 +6,40 @@ class Program
 {
     static void Main()
     {
+        var abc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var a = abc[0];
+        var b = abc[1];
+        var c = abc[2];
+
+        if (c % 2 == 0)
+        {
+            if (Math.Abs(a) > Math.Abs(b))
+            {
+                Console.WriteLine(">");
+            }
+            else if (Math.Abs(a) < Math.Abs(b))
+            {
+                Console.WriteLine("<");
+            }
+            else
+            {
+                Console.WriteLine("=");
+            }
+        }
+        else
+        {
+            if (a > b)
+            {
+                Console.WriteLine(">");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("<");
+            }
+            else
+            {
+                Console.WriteLine("=");
+            }
+        }
     }
 }
