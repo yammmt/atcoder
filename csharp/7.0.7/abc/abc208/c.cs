9a2ed55e16b426c0d2981ff466562a70551e4f35
yammmt <cu.cs.y20@gmail.com>
2025-08-04 22:00:35 +0900

    :tiger2:

--- csharp/7.0.7/abc/abc208/c.cs
+++ csharp/7.0.7/abc/abc208/c.cs
@@ -6,5 +6,31 @@ class Program
 {
     static void Main()
     {
+        var nk = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+        long n = nk[0];
+        long k = nk[1];
+        var an = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        var aAndI = new (int a, int i)[n];
+        for (int i = 0; i < n; i++)
+        {
+            aAndI[i] = (an[i], i);
+        }
+        Array.Sort(aAndI);
+
+        var ans = new long[n];
+        for (int i = 0; i < n; i++)
+        {
+            ans[i] = k / n;
+        }
+        for (int i = 0; i < k % n; i++)
+        {
+            ans[aAndI[i].i] += 1;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(ans[i]);
+        }
     }
 }
