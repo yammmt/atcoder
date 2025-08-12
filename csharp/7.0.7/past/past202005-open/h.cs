45e78e014ea7138d2b5097f0254fe9b9f13db4d6
yammmt <cu.cs.y20@gmail.com>
2025-08-12 21:15:20 +0900

    :x:

--- csharp/7.0.7/past/past202005-open/h.cs
+++ csharp/7.0.7/past/past202005-open/h.cs
@@ -1,8 +1,51 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        const int dummy = 1_000_000_000;
+        var nl = Console.ReadLine().Split(' ');
+        var n = int.Parse(nl[0]);
+        var l = int.Parse(nl[1]);
+        var xn = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        var t3 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        // 処理が楽なので bool 配列を用意して障害物情報を入れる
+        var hasHurdle = new bool[l + 1];
+        foreach (int x in xn)
+            hasHurdle[x] = true;
+
+        // 0.5 があると反射的に x2 したくなるが, しても変わらないような
+        // 最短時間を管理するとジャンプ中判定が難しくなるので, DP 配列二つ持つなど
+        var dp = new (int land, int air)[l + 1];
+        for (int i = 0; i < l + 1; i++)
+            dp[i] = (dummy, dummy);
+        dp[0].land = 0;
+
+        for (int i = 0; i < l; i++)
+        {
+            var pena = hasHurdle[i] ? t3[2] : 0;
+
+            // act 1
+            dp[i + 1].land = Math.Min(dp[i + 1].land, dp[i].land + t3[0] + pena);
+
+            // act 2
+            dp[i + 1].air = Math.Min(dp[i + 1].air, dp[i].land + t3[0] / 2 + t3[1] / 2 + pena);
+            if (i + 2 < l + 1)
+                dp[i + 2].land = Math.Min(dp[i + 2].land, dp[i].land + t3[0] + t3[1] + pena);
+
+            // act 3
+            // i + 1 は act 2 と同じ
+            if (i + 2 < l + 1)
+                dp[i + 2].air = Math.Min(dp[i + 2].air, dp[i].land + t3[0] / 2 + t3[1] * 3 / 2 + pena);
+            if (i + 3 < l + 1)
+                dp[i + 3].air = Math.Min(dp[i + 3].air, dp[i].land + t3[0] / 2 + t3[1] * 5 / 2 + pena);
+            if (i + 4 < l + 1)
+                dp[i + 4].land = Math.Min(dp[i + 4].land, dp[i].land + t3[0] + t3[1] * 3 + pena);
+        }
+
+        Console.WriteLine(Math.Min(dp[l].land, dp[l].air));
     }
 }
--- csharp/7.0.7/past/past202005-open/j.cs
+++ csharp/7.0.7/past/past202005-open/j.cs
@@ -1,8 +1,45 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        var nm = Console.ReadLine().Split(' ');
+        var n = int.Parse(nm[0]);
+        var m = int.Parse(nm[1]);
+        var am = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        // 子供が属した美味しさは降順に並ぶ, ので二分探索
+        // am[] の値より小さい最小/左の数を選ぶ
+        var deliciousness = new int[n];
+        foreach (int a in am)
+        {
+            var pass = n;
+            var fail = -1;
+            while (pass - fail > 1)
+            {
+                var mid = (pass + fail) / 2;
+                if (deliciousness[mid] < a)
+                {
+                    pass = mid;
+                }
+                else
+                {
+                    fail = mid;
+                }
+            }
+
+            if (pass == n)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(pass + 1);
+                deliciousness[pass] = a;
+            }
+        }
     }
 }
