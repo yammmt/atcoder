faf629361d8c89f72f11873794e4f999c8a064d3
yammmt <cu.cs.y20@gmail.com>
2025-08-15 17:55:07 +0900

    :zombie_woman:

--- csharp/7.0.7/past/past202005-open/f.cs
+++ csharp/7.0.7/past/past202005-open/f.cs
@@ -1,8 +1,56 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        const int ABC_NUM = 26;
+
+        var n = int.Parse(Console.ReadLine());
+        var ann = new char[n][];
+        for (int i = 0; i < n; i++)
+        {
+            ann[i] = Console.ReadLine().ToCharArray();
+        }
+
+        var ans = new char[n / 2];
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            var appeared = new bool[ABC_NUM];
+            for (int j = 0; j < n; j++)
+            {
+                var c = ann[i][j] - 'a';
+                appeared[c] = true;
+            }
+
+            var passed = false;
+            for (int j = 0; j < n; j++)
+            {
+                var c = ann[n - i - 1][j] - 'a';
+                if (appeared[c])
+                {
+                    ans[i] = ann[n - i - 1][j];
+                    passed = true;
+                    break;
+                }
+            }
+
+            if (!passed)
+            {
+                Console.WriteLine(-1);
+                return;
+            }
+        }
+
+        Console.Write(new string(ans));
+        if (n % 2 == 1)
+        {
+            Console.Write(ann[n / 2][0]);
+        }
+        Array.Reverse(ans);
+        Console.WriteLine(new string(ans));
     }
 }
