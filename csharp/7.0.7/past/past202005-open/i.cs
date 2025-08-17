46920a30303b85f44c84d8154c10410515c427d4
yammmt <cu.cs.y20@gmail.com>
2025-08-17 12:17:32 +0900

    :business_suit_levitating:

--- csharp/7.0.7/past/past202005-open/i.cs
+++ csharp/7.0.7/past/past202005-open/i.cs
@@ -1,8 +1,72 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
+    static void Swap(ref long a, ref long b)
+    {
+        (a, b) = (b, a);
+    }
+
     static void Main()
     {
+        var n = long.Parse(Console.ReadLine());
+        var q = int.Parse(Console.ReadLine());
+
+        var rows = new long[n];
+        var columns = new long[n];
+        for (int i = 0; i < n; i++)
+        {
+            rows[i] = i;
+            columns[i] = i;
+        }
+        bool isTransposed = false;
+
+        for (int i = 0; i < q; i++)
+        {
+            var query = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            switch (query[0])
+            {
+                case 1:
+                    // 行の交換
+                    var qa1 = query[1] - 1;
+                    var qb1 = query[2] - 1;
+                    if (isTransposed)
+                    {
+                        Swap(ref columns[qa1], ref columns[qb1]);
+                    }
+                    else
+                    {
+                        Swap(ref rows[qa1], ref rows[qb1]);
+                    }
+                    break;
+                case 2:
+                    // 列の交換
+                    var qa2 = query[1] - 1;
+                    var qb2 = query[2] - 1;
+                    if (isTransposed)
+                    {
+                        Swap(ref rows[qa2], ref rows[qb2]);
+                    }
+                    else
+                    {
+                        Swap(ref columns[qa2], ref columns[qb2]);
+                    }
+                    break;
+                case 3:
+                    isTransposed = !isTransposed;
+                    break;
+                case 4:
+                    var qa4 = query[1] - 1;
+                    var qb4 = query[2] - 1;
+                    var iRow = isTransposed ? qb4 : qa4;
+                    var iCol = isTransposed ? qa4 : qb4;
+                    Console.WriteLine(n * rows[iRow] + columns[iCol]);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
--- csharp/7.0.7/past/past202005-open/k.cs
+++ csharp/7.0.7/past/past202005-open/k.cs
@@ -4,5 +4,52 @@ class Program
 {
     static void Main()
     {
+        const int none = -1;
+
+        var nq = Console.ReadLine().Split(' ');
+        var n = int.Parse(nq[0]);
+        var q = int.Parse(nq[1]);
+
+        // 愚直に考えると, 各机の上にコンテナを線形リストでもつ
+        // 移動開始時に机の情報が与えられるので, コンテナがどこにあるかは机を辿れば O(N) でわかる
+        // 愚直だと O(NQ) になる. でもコンテナ初期値の都合で高さが均されるのでは？
+        // 高さ見る回数が高々 2Q 回かそこらに収まりそう, 本当に？
+
+        var containerLower = new int[n];
+        Array.Fill(containerLower, none);
+        var containerHighest = new int[n];
+        for (int i = 0; i < n; i++)
+            containerHighest[i] = i;
+
+        for (int i = 0; i < q; i++)
+        {
+            var ftx = Console.ReadLine().Split(' ');
+            var f = int.Parse(ftx[0]) - 1;
+            var t = int.Parse(ftx[1]) - 1;
+            var x = int.Parse(ftx[2]) - 1;
+
+            // 移動先の最上位
+            var highestPrev = containerHighest[t];
+            containerHighest[t] = containerHighest[f];
+
+            // 移動元の最上位
+            containerHighest[f] = containerLower[x];
+
+            // 移動先のひとつ下
+            containerLower[x] = highestPrev;
+        }
+
+        // 一度全体を舐めてコンテナの位置を取得する
+        var ans = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            var j = containerHighest[i];
+            while (j != none)
+            {
+                ans[j] = i + 1;
+                j = containerLower[j];
+            }
+        }
+        Console.WriteLine(string.Join("\n", ans));
     }
 }
